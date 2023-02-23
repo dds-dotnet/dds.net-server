0083012535da01ef9f528cb0ed0cd048dbfedacd
usama <mahmoodify@gmail.com>
2023-02-23 17:17:55 +0500

    moving default config inside the server lib

--- src/dds.net-server.lib/Configuration/DefaultConfig.cs
+++ src/dds.net-server.lib/Configuration/DefaultConfig.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DDS.Net.Server.WpfApp.Configuration
+namespace DDS.Net.Server.Configuration
 {
-    class DefaultConfiguration
+    internal class DefaultConfig
     {
     }
 }

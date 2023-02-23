5c4a1f53156b081bfbe876b7059452e01ac464d7
usama <mahmoodify@gmail.com>
2023-02-23 18:04:46 +0500

    adding path parts extensions

---
+++ src/dds.net-server.lib/Extensions/PathExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace DDS.Net.Server.Extensions
+{
+    internal static class PathExtensions
+    {
+        public static List<string> TrimmedParts(this string str, char splitter)
+        {
+            if (string.IsNullOrEmpty(str) == false)
+            {
+                string[] parts = str.Split(splitter, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length > 0)
+                {
+                    return new List<string>(parts);
+                }
+            }
+
+            return new List<string>();
+        }
+    }
+}

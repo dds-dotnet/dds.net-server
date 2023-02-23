384f9facb283f4466c96fbcc17db7edb38cf89f2
usama <mahmoodify@gmail.com>
2023-02-23 21:27:29 +0500

    cleanup

--- src/dds.net-server.app.wpf/AppWindows/MainWindow.xaml.cs
+++ src/dds.net-server.app.wpf/AppWindows/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace DDS.Net.Server.WpfApp.AppWindows
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()

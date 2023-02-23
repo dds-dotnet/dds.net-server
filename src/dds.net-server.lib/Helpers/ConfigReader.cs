13f5d565ae86e7c8dfa035db30ce342bed83c3c3
usama <mahmoodify@gmail.com>
2023-02-23 17:56:02 +0500

    adding default getters

--- src/dds.net-server.lib/Helpers/ConfigReader.cs
+++ src/dds.net-server.lib/Helpers/ConfigReader.cs
@@ -8,5 +8,31 @@ namespace DDS.Net.Server.Helpers
 {
     public class ConfigReader
     {
+        public string Filename { get; set; }
+
+        public ConfigReader(string filename)
+        {
+            Filename = filename;
+        }
+
+        public string GetStringValue(string key, string defaultValue = "")
+        {
+            return defaultValue;
+        }
+
+        public int GetIntegerValue(string key, int defaultValue = -1)
+        {
+            return defaultValue;
+        }
+
+        public float GetFloatValue(string key, float defaultValue = 0)
+        {
+            return defaultValue;
+        }
+
+        public double GetDoubleValue(string key, double defaultValue = 0)
+        {
+            return defaultValue;
+        }
     }
 }

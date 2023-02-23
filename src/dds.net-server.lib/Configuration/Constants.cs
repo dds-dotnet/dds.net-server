5f0f4e444a9e59d453aaffab5305c256a17cd202
usama <mahmoodify@gmail.com>
2023-02-23 21:39:57 +0500

    adding config file path

--- src/dds.net-server.lib/Configuration/Constants.cs
+++ src/dds.net-server.lib/Configuration/Constants.cs
@@ -8,5 +8,6 @@ namespace DDS.Net.Server.Configuration
 {
     internal static class Constants
     {
+        static public readonly string SERVER_CONFIG_FILENAME = "Configuration/server.ini";
     }
 }

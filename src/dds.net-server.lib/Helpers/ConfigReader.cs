96185ca98e1612661806b68823424dc2eb0f6cff
usama <mahmoodify@gmail.com>
2023-02-23 18:15:08 +0500

    updating help and section getter

--- src/dds.net-server.lib/Helpers/ConfigReader.cs
+++ src/dds.net-server.lib/Helpers/ConfigReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,26 +11,59 @@ namespace DDS.Net.Server.Helpers
     {
         public string Filename { get; set; }
 
+        private Dictionary<string, Dictionary<string, string>> _config;
+
         public ConfigReader(string filename)
         {
             Filename = filename;
+            _config = new Dictionary<string, Dictionary<string, string>>();
+
+            if (File.Exists(Filename) == false) return;
         }
 
+        private Dictionary<string, string>? GetSection(string sectionName)
+        {
+            if (_config.ContainsKey(sectionName)) return _config[sectionName];
+
+            return null;
+        }
+
+        /// <summary>
+        /// Getting a string value from INI file
+        /// </summary>
+        /// <param name="key">Section Name / Property Name</param>
+        /// <param name="defaultValue">Return the value when key is not found</param>
+        /// <returns></returns>
         public string GetString(string key, string defaultValue = "")
         {
             return defaultValue;
         }
-
+        /// <summary>
+        /// Getting an integer value from INI file
+        /// </summary>
+        /// <param name="key">Section Name / Property Name</param>
+        /// <param name="defaultValue">Return the value when key is not found</param>
+        /// <returns></returns>
         public int GetInteger(string key, int defaultValue = -1)
         {
             return defaultValue;
         }
-
+        /// <summary>
+        /// Getting a float value from INI file
+        /// </summary>
+        /// <param name="key">Section Name / Property Name</param>
+        /// <param name="defaultValue">Return the value when key is not found</param>
+        /// <returns></returns>
         public float GetFloat(string key, float defaultValue = 0)
         {
             return defaultValue;
         }
-
+        /// <summary>
+        /// Getting a double value from INI file
+        /// </summary>
+        /// <param name="key">Section Name / Property Name</param>
+        /// <param name="defaultValue">Return the value when key is not found</param>
+        /// <returns></returns>
         public double GetDouble(string key, double defaultValue = 0)
         {
             return defaultValue;

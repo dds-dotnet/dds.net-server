03288d3b97e3b477bd259cd283de005cce425613
usama <mahmoodify@gmail.com>
2023-02-23 21:43:15 +0500

    adding iface methods

--- src/dds.net-server.lib/Interfaces/ILogger.cs
+++ src/dds.net-server.lib/Interfaces/ILogger.cs
@@ -8,5 +8,8 @@ namespace DDS.Net.Server.Interfaces
 {
     internal interface ILogger
     {
+        void Info(string message);
+        void Warning(string message);
+        void Error(string message);
     }
 }

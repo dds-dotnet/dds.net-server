d4d6c83e24d370bc46cc7f53687e2e5109d9839b
usama <mahmoodify@gmail.com>
2023-02-23 21:50:22 +0500

    adding blank logger

---
+++ src/dds.net-server.lib/Interfaces/DefaultImplementations/BlankLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDS.Net.Server.Interfaces.DefaultImplementations
+{
+    internal class BlankLogger : ILogger
+    {
+        public void Error(string message)
+        {
+        }
+
+        public void Info(string message)
+        {
+        }
+
+        public void Warning(string message)
+        {
+        }
+    }
+}
---
+++ src/dds.net-server.lib/Services/DependencyService.cs
@@ -0,0 +1,19 @@
+﻿using DDS.Net.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDS.Net.Server.Services
+{
+    internal static class DependencyService
+    {
+        private static ILogger _logger = null;
+
+        public static ILogger GetLogger()
+        {
+            return _logger;
+        }
+    }
+}

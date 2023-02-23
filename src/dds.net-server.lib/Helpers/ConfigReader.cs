5290298643eeffaa7c0b280ad87bfc046b777e1b
usama <mahmoodify@gmail.com>
2023-02-23 19:05:15 +0500

    config reading completed

--- src/dds.net-server.lib/Helpers/ConfigReader.cs
+++ src/dds.net-server.lib/Helpers/ConfigReader.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DDS.Net.Server.Helpers
@@ -19,7 +20,75 @@ namespace DDS.Net.Server.Helpers
             Filename = filename;
             _config = new Dictionary<string, Dictionary<string, string>>();
 
+            LoadFile();
+        }
+
+        private void LoadFile()
+        {
+            _config.Clear();
+
             if (File.Exists(Filename) == false) return;
+
+            Regex sectionPattern = new Regex(@"^\s*\[([a-zA-Z0-9\s-]+)\]\s*$");
+            Regex propertyPattern = new Regex(@"^\s*([a-zA-Z0-9\s-]+)\s*=+\s*([a-zA-Z0-9\s-]+)\s*$");
+
+            string? currentSection = null;
+
+            using (StreamReader stream = File.OpenText(Filename))
+            {
+                while (stream.EndOfStream == false)
+                {
+                    string? line = stream.ReadLine();
+
+                    if (string.IsNullOrEmpty(line) == false)
+                    {
+                        string trimmedLine = line.Trim();
+
+                        if (trimmedLine.StartsWith('['))
+                        {
+                            currentSection = null;
+
+                            if (sectionPattern.IsMatch(trimmedLine))
+                            {
+                                currentSection = sectionPattern.Match(trimmedLine).Groups[1].Value;
+                            }
+                        }
+                        else
+                        {
+                            if (currentSection != null && propertyPattern.IsMatch(trimmedLine))
+                            {
+                                Match match = propertyPattern.Match(trimmedLine);
+
+                                string property = match.Groups[1].Value.Trim();
+                                string value = match.Groups[2].Value.Trim();
+
+                                InsertValueInConfiguration(currentSection, property, value);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void InsertValueInConfiguration(string sectionName, string propertyName, string propertyValue)
+        {
+            Dictionary<string, string>? section = GetSection(sectionName);
+
+            if (section == null)
+            {
+                section = new Dictionary<string, string>();
+                _config.Add(sectionName, section);
+            }
+
+            if (section.ContainsKey(propertyName) == false)
+            {
+                section.Add(propertyName, propertyValue);
+            }
+            else
+            {
+                section[propertyName] = propertyValue;
+            }
         }
 
         private Dictionary<string, string>? GetSection(string sectionName)

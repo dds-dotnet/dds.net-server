2d60d46caa5a4942b25aa8a4cf97164613cf0cba
usama <mahmoodify@gmail.com>
2023-02-23 16:45:05 +0500

    updating ns

--- src/dds.net-server.app.wpf/App.xaml.cs
+++ src/dds.net-server.app.wpf/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace dds.net.server.wpfapp
+namespace DDS.Net.Server.WpfApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- src/dds.net-server.app.wpf/AppWindows/MainWindow.xaml.cs
+++ src/dds.net-server.app.wpf/AppWindows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace dds.net.server.wpfapp.AppWindows
+namespace DDS.Net.Server.WpfApp.AppWindows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml

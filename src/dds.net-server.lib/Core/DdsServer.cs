ff8e90a1d9659fefeaaa8600e7c7fb43953204f1
usama <mahmoodify@gmail.com>
2023-02-24 12:45:47 +0500

    adding ctor with conf

--- src/dds.net-server.lib/Core/DdsServer.cs
+++ src/dds.net-server.lib/Core/DdsServer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DDS.Net.Server.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace DDS.Net.Server
 {
-    internal class DdsServer
+    public class DdsServer
     {
+        private readonly ServerConfiguration _configuration;
+
+        public DdsServer(ServerConfiguration configuration)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            _configuration = configuration;
+
+            if (_configuration.Logger == null)
+                throw new Exception($"No {nameof(_configuration.Logger)} instance is provided");
+        }
     }
 }

29f65f749ef15a55712a315a11c55789345094d2
usama <mahmoodify@gmail.com>
2023-02-24 16:55:30 +0500

    adding max clients for udp and tcp

--- src/dds.net-server.lib/Entities/ServerConfiguration.cs
+++ src/dds.net-server.lib/Entities/ServerConfiguration.cs
@@ -8,23 +8,30 @@ namespace DDS.Net.Server.Entities
 
         public bool EnableTCP { get; }
         public ushort ListeningPortTCP { get; }
+        public int MaxClientsTCP { get; }
 
         public bool EnableUDP { get; }
         public ushort ListeningPortUDP { get; }
+        public int MaxClientsUDP { get; }
 
         public ILogger Logger { get; }
 
         public ServerConfiguration(
             string listeningIPv4Address,
-            bool enableTCP, ushort tcpPort,
-            bool enableUDP, ushort udpPort,
+            bool enableTCP, ushort tcpPort, int tcpMaxClients,
+            bool enableUDP, ushort udpPort, int udpMaxClients,
             ILogger logger)
         {
             ListeningAddressIPv4 = listeningIPv4Address;
+
             EnableTCP = enableTCP;
             ListeningPortTCP = tcpPort;
+            MaxClientsTCP = tcpMaxClients;
+
             EnableUDP = enableUDP;
             ListeningPortUDP = udpPort;
+            MaxClientsUDP = udpMaxClients;
+
             Logger = logger;
         }
     }

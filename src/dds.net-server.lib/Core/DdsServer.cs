837b84a6942c29c844f25b25a89bc161fa9fe8aa
usama <mahmoodify@gmail.com>
2023-02-24 12:47:40 +0500

    adding local for logger

--- src/dds.net-server.lib/Core/DdsServer.cs
+++ src/dds.net-server.lib/Core/DdsServer.cs
@@ -1,4 +1,5 @@
 ﻿using DDS.Net.Server.Entities;
+using DDS.Net.Server.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,19 @@ namespace DDS.Net.Server
 {
     public class DdsServer
     {
-        private readonly ServerConfiguration _configuration;
+        private readonly ServerConfiguration _config;
+        private readonly ILogger _logger;
 
         public DdsServer(ServerConfiguration configuration)
         {
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
-            _configuration = configuration;
+            _config = configuration;
 
-            if (_configuration.Logger == null)
-                throw new Exception($"No {nameof(_configuration.Logger)} instance is provided");
+            if (_config.Logger == null)
+                throw new Exception($"No {nameof(_config.Logger)} instance is provided");
+
+            _logger = _config.Logger;
         }
     }
 }

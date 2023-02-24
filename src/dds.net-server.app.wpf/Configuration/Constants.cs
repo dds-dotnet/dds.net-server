1d04b354b2c285fcf47140c0c13345edaf9ad1fb
usama <mahmoodify@gmail.com>
2023-02-24 23:05:05 +0500

    adding timestamp to log filename

--- src/dds.net-server.app.wpf/Configuration/Constants.cs
+++ src/dds.net-server.app.wpf/Configuration/Constants.cs
@@ -1,8 +1,15 @@
-﻿namespace DDS.Net.Server.WpfApp.Configuration
+﻿using System;
+
+namespace DDS.Net.Server.WpfApp.Configuration
 {
     internal static class Constants
     {
-        static public readonly string LOG_FILENAME = "Configuration/log.txt";
+        static private readonly DateTime _TIMESTAMP = DateTime.Now;
+        static public readonly string _TIMESTAMP_TEXT =
+            $"{_TIMESTAMP.Year}_{_TIMESTAMP.Month,02:00}_{_TIMESTAMP.Day,02:00}_" +
+            $"{_TIMESTAMP.Hour,02:00}_{_TIMESTAMP.Minute,02:00}_{_TIMESTAMP.Second,02:00}";
+
+        static public readonly string LOG_FILENAME = $"Log/log-{_TIMESTAMP_TEXT}.txt";
         static public readonly string SERVER_01_CONFIG_FILENAME = "Configuration/server-01.ini";
     }
 }

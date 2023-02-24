cc05d6eaafb1eb5344f0c13e154607df67282e3e
usama <mahmoodify@gmail.com>
2023-02-24 19:30:36 +0500

    checking ipv4

--- src/dds.net-server.lib/Core/Internal/BaseServer.cs
+++ src/dds.net-server.lib/Core/Internal/BaseServer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DDS.Net.Server.Core.Internal
@@ -22,6 +23,19 @@ namespace DDS.Net.Server.Core.Internal
             _maxClients = maxClients;
 
             _logger = logger;
+
+            Regex ipv4Pattern = new Regex(@"\s*(\d{1,3})\s*\.\s*(\d{1,3})\s*\.\s*(\d{1,3})\s*\.\s*(\d{1,3})\s*");
+
+            if (string.IsNullOrEmpty(IPv4) ||
+                IPv4.ToLower().Contains("any"))
+            {
+                _IPv4 = "0.0.0.0";
+            }
+            else if (ipv4Pattern.IsMatch(IPv4) == false)
+            {
+                logger.Warning($"Invalid IPv4 Address: \"{IPv4}\", using 0.0.0.0 instead");
+                _IPv4 = "0.0.0.0";
+            }
         }
 
         public abstract void StartServer();

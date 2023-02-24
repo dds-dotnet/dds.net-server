17727d3c6317a8c247067e7e68cccafa09064a2e
usama <mahmoodify@gmail.com>
2023-02-24 17:44:21 +0500

    adding blank logger - to help with splitting initially

---
+++ src/dds.net-server.app.wpf/Interfaces/Logger/BlankLogger.cs
@@ -0,0 +1,19 @@
+﻿using DDS.Net.Server.Interfaces;
+
+namespace DDS.Net.Server.WpfApp.Interfaces.Logger
+{
+    internal class BlankLogger : ILogger
+    {
+        public void Error(string message)
+        {
+        }
+
+        public void Info(string message)
+        {
+        }
+
+        public void Warning(string message)
+        {
+        }
+    }
+}

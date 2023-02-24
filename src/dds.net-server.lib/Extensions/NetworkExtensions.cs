94124a489807c476f73c4d108b897a7a95f6d767
usama <mahmoodify@gmail.com>
2023-02-25 00:35:23 +0500

    adding extensions to server

---
+++ src/dds.net-server.lib/Extensions/NetworkExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace DDS.Net.Server.Extensions
+{
+    internal static class NetworkExtensions
+    {
+        public static bool IsIPAddressAssignedToAnUpInterface(this string ipAddress)
+        {
+            NetworkInterface[] ifaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            bool isAssigned = false;
+
+            if (ifaces != null && ifaces.Length > 0)
+            {
+                foreach (NetworkInterface iface in ifaces)
+                {
+                    if (iface.OperationalStatus == OperationalStatus.Up)
+                    {
+                        foreach (
+                            UnicastIPAddressInformation addressInfo
+                            in
+                            iface.GetIPProperties().UnicastAddresses)
+                        {
+                            if (addressInfo.Address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                if (addressInfo.Address.ToString() == ipAddress)
+                                {
+                                    isAssigned = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (isAssigned)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return isAssigned;
+        }
+    }
+}

9432b924d036cf4a2f7384adaa62fe33205894e9
usama <mahmoodify@gmail.com>
2023-02-24 12:20:23 +0500

    adding basic config

--- src/dds.net-server.lib/Entities/ServerConfiguration.cs
+++ src/dds.net-server.lib/Entities/ServerConfiguration.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DDS.Net.Server.Entities
+﻿namespace DDS.Net.Server.Entities
 {
     public class ServerConfiguration
     {
+        public string ListeningAddressIPv4 { get; }
+
+        public bool EnableTCP { get; }
+        public ushort ListeningPortTCP { get; }
+
+        public bool EnableUDP { get; }
+        public ushort ListeningPortUDP { get; }
+
+        public ServerConfiguration(
+            string listeningIPv4Address,
+            bool enableTCP, ushort tcpPort,
+            bool enableUDP, ushort udpPort)
+        {
+            ListeningAddressIPv4 = listeningIPv4Address;
+            EnableTCP = enableTCP;
+            ListeningPortTCP = tcpPort;
+            EnableUDP = enableUDP;
+            ListeningPortUDP = udpPort;
+        }
     }
 }

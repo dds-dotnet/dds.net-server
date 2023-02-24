760a57cc7b81d248733a6c29abdf88cfefbf433c
usama <mahmoodify@gmail.com>
2023-02-24 15:17:21 +0500

    formattingadding events placeholder

---
+++ src/dds.net-server.lib/Core/DdsServer-Events.cs
@@ -0,0 +1,15 @@
+﻿using DDS.Net.Server.Entities;
+using DDS.Net.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDS.Net.Server
+{
+    public partial class DdsServer
+    {
+
+    }
+}

296bc7dad7a92e6dd747287e6b2ebd76e5ae8af7
usama <mahmoodify@gmail.com>
2023-02-24 17:51:20 +0500

    adding file logger

---
+++ src/dds.net-server.app.wpf/Interfaces/Logger/FileLogger.cs
@@ -0,0 +1,60 @@
+﻿using DDS.Net.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDS.Net.Server.WpfApp.Interfaces.Logger
+{
+    internal class FileLogger : ILogger, IDisposable
+    {
+        private readonly LogLevel _logLevel;
+        private StreamWriter? _writer;
+
+        public FileLogger(LogLevel logLevel, string filename)
+        {
+            try
+            {
+                _writer = File.CreateText(filename);
+            }
+            catch (Exception ex)
+            {
+                _writer = null;
+                throw new Exception($"Cannot write logfile \"{filename}\" - {ex.Message}");
+            }
+
+            _logLevel = logLevel;
+        }
+
+        public void Dispose()
+        {
+            if (_writer != null)
+            {
+                _writer.Dispose();
+            }
+        }
+
+        public void Error(string message)
+        {
+            _writer?.WriteLine($"Error: {message}");
+        }
+
+        public void Info(string message)
+        {
+            if (_logLevel == LogLevel.Information)
+            {
+                _writer?.WriteLine(message);
+            }
+        }
+
+        public void Warning(string message)
+        {
+            if (_logLevel != LogLevel.Error)
+            {
+                _writer?.WriteLine($"Warning: {message}");
+            }
+        }
+    }
+}

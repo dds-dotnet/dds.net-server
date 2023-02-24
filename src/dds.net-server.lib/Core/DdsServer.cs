26dba4b00941bb94c7cca83d82fb69fc5b8c6c75
usama <mahmoodify@gmail.com>
2023-02-24 15:44:44 +0500

    adding internal tcp and udp servers

--- src/dds.net-server.lib/Core/DdsServer.cs
+++ src/dds.net-server.lib/Core/DdsServer.cs
@@ -1,3 +1,4 @@
+﻿using DDS.Net.Server.Core.Internal;
 ﻿using DDS.Net.Server.Entities;
 using DDS.Net.Server.Interfaces;
 using System;
@@ -22,12 +23,16 @@ namespace DDS.Net.Server
         private readonly ILogger _logger;
 
         private ServerStatus _status = ServerStatus.Stopped;
+        private TcpServer? _tcpServer;
+        private UdpServer? _udpServer;
 
         public DdsServer(ServerConfiguration config)
         {
             if (config == null) throw new ArgumentNullException(nameof(config));
 
             _config = config;
+            _tcpServer = null;
+            _udpServer = null;
 
             if (_config.Logger != null)
                 _logger = _config.Logger;

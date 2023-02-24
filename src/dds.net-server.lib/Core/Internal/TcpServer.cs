2d4fba17fed3517d9d8eaa0f8e2e2de0f5a688bb
usama <mahmoodify@gmail.com>
2023-02-24 15:30:43 +0500

    adding blank tcp server

---
+++ src/dds.net-server.lib/Core/Internal/TcpServer.cs
@@ -0,0 +1,23 @@
+﻿using DDS.Net.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDS.Net.Server.Core.Internal
+{
+    internal class TcpServer
+    {
+        private readonly string _IPv4;
+        private readonly ushort _port;
+        private readonly ILogger _logger;
+
+        public TcpServer(string IPv4, ushort port, ILogger logger)
+        {
+            _IPv4 = IPv4;
+            this._port = port;
+            this._logger = logger;
+        }
+    }
+}

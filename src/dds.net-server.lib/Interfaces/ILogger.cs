f9d9a494b27b629fe0ab596ab35a581297114ddd
usama <mahmoodify@gmail.com>
2023-02-24 17:47:59 +0500

    adding log level

--- src/dds.net-server.lib/Interfaces/ILogger.cs
+++ src/dds.net-server.lib/Interfaces/ILogger.cs
@@ -1,5 +1,10 @@
 ﻿namespace DDS.Net.Server.Interfaces
 {
+    public enum LogLevel
+    {
+        Information, Warning, Error
+    }
+
     public interface ILogger
     {
         void Info(string message);

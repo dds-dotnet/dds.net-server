521f3d41a64c9ce35b6db9c527c249d2333d3e39
usama <mahmoodify@gmail.com>
2023-02-24 18:05:22 +0500

    adding log filename

--- src/dds.net-server.app.wpf/Configuration/Constants.cs
+++ src/dds.net-server.app.wpf/Configuration/Constants.cs
@@ -2,6 +2,7 @@
 {
     internal static class Constants
     {
+        static public readonly string LOG_FILENAME = "Configuration/log.txt";
         static public readonly string SERVER_01_CONFIG_FILENAME = "Configuration/server-01.ini";
     }
 }

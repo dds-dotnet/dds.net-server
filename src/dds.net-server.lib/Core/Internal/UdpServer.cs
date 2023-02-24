3edad2eea3293df1cdbbfb5f1176390eaa6d8055
usama <mahmoodify@gmail.com>
2023-02-24 15:32:01 +0500

    ading udp server

---
+++ src/dds.net-server.lib/Core/Internal/UdpServer.cs
@@ -0,0 +1,23 @@
+﻿using DDS.Net.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDS.Net.Server.Core.Internal
+{
+    internal class UdpServer
+    {
+        private readonly string _IPv4;
+        private readonly ushort _port;
+        private readonly ILogger _logger;
+
+        public UdpServer(string IPv4, ushort port, ILogger logger)
+        {
+            _IPv4 = IPv4;
+            _port = port;
+            _logger = logger;
+        }
+    }
+}

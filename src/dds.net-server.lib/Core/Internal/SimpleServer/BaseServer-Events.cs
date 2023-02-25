29e08b3f356ce2035043b67da4e6bb22dc2c2f1f
usama <mahmoodify@gmail.com>
2023-02-25 21:55:04 +0500

    cleaning-up and adding status changed event

--- src/dds.net-server.lib/Core/Internal/SimpleServer/BaseServer-Events.cs
+++ src/dds.net-server.lib/Core/Internal/SimpleServer/BaseServer-Events.cs
@@ -1,20 +1,9 @@
-﻿using DDS.Net.Server.Core.Internal.Extensions;
-using DDS.Net.Server.Core.Internal.SimpleServer.Types;
-using DDS.Net.Server.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Net.Sockets;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using DDS.Net.Server.Core.Internal.SimpleServer.Types;
 
 namespace DDS.Net.Server.Core.Internal.SimpleServer
 {
     internal abstract partial class BaseServer
     {
-        
+        public event EventHandler<SimpleServerStatus>? ServerStatusChanged;
     }
 }

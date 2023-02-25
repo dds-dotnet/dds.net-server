c40ef6d67d680d9814ca2d227f5e5d8f206ebb18
usama <mahmoodify@gmail.com>
2023-02-25 22:01:08 +0500

    creating simple impl

--- src/dds.net-server.lib/Core/Internal/SimpleServer/Types/SimpleServerClientPacket.cs
+++ src/dds.net-server.lib/Core/Internal/SimpleServer/Types/SimpleServerClientPacket.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
 
 namespace DDS.Net.Server.Core.Internal.SimpleServer.Types
 {
     internal class SimpleServerClientPacket
     {
+        public IPEndPoint ClientInfo { get; }
+        public byte[] PacketData { get; }
+
+        public SimpleServerClientPacket(IPEndPoint from, byte[] data)
+        {
+            ClientInfo = from;
+            PacketData = data;
+        }
     }
 }

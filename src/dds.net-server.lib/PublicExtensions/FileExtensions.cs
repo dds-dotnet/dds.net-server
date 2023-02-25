c6e91d2c1f507184d7bcda3316ceac033f7a5b18
usama <mahmoodify@gmail.com>
2023-02-25 16:10:01 +0500

    moving extensiosn

---
+++ src/dds.net-server.lib/PublicExtensions/FileExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace DDS.Net.Server.PublicExtensions
+{
+    public static class FileExtensions
+    {
+        public static List<string> TrimmedParts(this string str, char splitter)
+        {
+            if (string.IsNullOrEmpty(str) == false)
+            {
+                string[] parts = str.Split(splitter, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length > 0)
+                {
+                    return new List<string>(parts);
+                }
+            }
+
+            return new List<string>();
+        }
+    }
+}

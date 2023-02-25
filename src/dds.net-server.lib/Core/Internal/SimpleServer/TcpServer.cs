15fc49be8080c50ec9c5846535589107106447a7
usama <mahmoodify@gmail.com>
2023-02-25 21:26:45 +0500

    starting and stopping the server thread

--- src/dds.net-server.lib/Core/Internal/SimpleServer/TcpServer.cs
+++ src/dds.net-server.lib/Core/Internal/SimpleServer/TcpServer.cs
@@ -9,19 +9,57 @@ namespace DDS.Net.Server.Core.Internal.SimpleServer
 {
     internal class TcpServer : BaseServer
     {
+        private enum ServerStatus
+        {
+            Stopped,
+            Running
+        }
+
+        private volatile ServerStatus status;
+        private volatile bool isConnectionListenerThreadRunning = false;
+        private Thread? connectionListenerThread = null;
+
         public TcpServer(string IPv4, ushort port, int maxClients, ILogger logger)
             : base(IPv4, port, maxClients, ServerType.TCP, logger)
         {
+            status = ServerStatus.Stopped;
         }
 
         public override void StartServer()
         {
-            throw new NotImplementedException();
+            lock (this)
+            {
+                if (connectionListenerThread == null)
+                {
+                    isConnectionListenerThreadRunning = true;
+                    connectionListenerThread = new Thread(ConnectionListenerThreadFunction);
+                    connectionListenerThread.IsBackground = true;
+                    connectionListenerThread.Start();
+                }
+            }
         }
 
         public override void StopServer()
         {
-            throw new NotImplementedException();
+            lock (this)
+            {
+                if (connectionListenerThread != null)
+                {
+                    isConnectionListenerThreadRunning = false;
+                    connectionListenerThread.Join();
+                }
+            }
+        }
+
+        private void ConnectionListenerThreadFunction()
+        {
+            while (isConnectionListenerThreadRunning)
+            {
+
+            }
+
+            isConnectionListenerThreadRunning = false;
+            connectionListenerThread = null;
         }
     }
 }

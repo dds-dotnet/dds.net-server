d7db8a62525840e7db0ca2786285227b1aa179a6
usama <mahmoodify@gmail.com>
2023-02-25 18:59:13 +0500

    adding extension method for checking any of the text

--- src/dds.net-server.lib/Core/Internal/Extensions/StringExtensions.cs
+++ src/dds.net-server.lib/Core/Internal/Extensions/StringExtensions.cs
@@ -8,6 +8,12 @@ namespace DDS.Net.Server.Core.Internal.Extensions
 {
     internal static class StringExtensions
     {
-
+        public static bool IsContainingAnyIgnoringCase(this string text,
+            string text01,
+            string text02)
+        {
+            return text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text02, StringComparison.CurrentCultureIgnoreCase);
+        }
     }
 }

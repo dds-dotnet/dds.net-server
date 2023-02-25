cd457c618835038e799602fe912a017abb89d5c4
usama <mahmoodify@gmail.com>
2023-02-25 22:33:58 +0500

    separating the types

--- src/dds.net-server.lib/Core/Internal/SimpleServer/Types/SimpleServerPacket.cs
+++ src/dds.net-server.lib/Core/Internal/SimpleServer/Types/SimpleServerPacket.cs
@@ -2,12 +2,6 @@
 
 namespace DDS.Net.Server.Core.Internal.SimpleServer.Types
 {
-    internal enum SimpleServerPacketSendingStatus
-    {
-        Successful,
-        Failed
-    }
-
     internal class SimpleServerPacket
     {
         public IPEndPoint ClientInfo { get; }

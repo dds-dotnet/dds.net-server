6c472840354fbb01893d31a84c044ece25df917b
usama <mahmoodify@gmail.com>
2023-02-25 19:01:44 +0500

    adding multiple versions

--- src/dds.net-server.lib/Core/Internal/Extensions/StringExtensions.cs
+++ src/dds.net-server.lib/Core/Internal/Extensions/StringExtensions.cs
@@ -15,5 +15,41 @@ namespace DDS.Net.Server.Core.Internal.Extensions
             return text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
                    text.Contains(text02, StringComparison.CurrentCultureIgnoreCase);
         }
+
+        public static bool IsContainingAnyIgnoringCase(this string text,
+            string text01,
+            string text02,
+            string text03)
+        {
+            return text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text02, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text03, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public static bool IsContainingAnyIgnoringCase(this string text,
+            string text01,
+            string text02,
+            string text03,
+            string text04)
+        {
+            return text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text02, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text03, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text04, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public static bool IsContainingAnyIgnoringCase(this string text,
+            string text01,
+            string text02,
+            string text03,
+            string text04,
+            string text05)
+        {
+            return text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text02, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text03, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text04, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text05, StringComparison.CurrentCultureIgnoreCase);
+        }
     }
 }

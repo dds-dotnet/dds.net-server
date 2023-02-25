71b3af62fed4810860fc1f202216bf4bafc593ec
usama <mahmoodify@gmail.com>
2023-02-25 22:06:21 +0500

    creating basic arch

--- src/dds.net-server.lib/Core/Internal/SimpleServer/UdpServer.cs
+++ src/dds.net-server.lib/Core/Internal/SimpleServer/UdpServer.cs
@@ -10,19 +10,54 @@ namespace DDS.Net.Server.Core.Internal.SimpleServer
 {
     internal class UdpServer : BaseServer
     {
+        private volatile ServerStatus status;
+        private volatile bool isClientListenerThreadRunning = false;
+        private Thread? clientListenerThread = null;
+
         public UdpServer(string IPv4, ushort port, int maxClients, ILogger logger)
             : base(IPv4, port, maxClients, SimpleServerType.UDP, logger)
         {
+            status = ServerStatus.Stopped;
         }
 
         public override void StartServer()
         {
-            throw new NotImplementedException();
+            lock (this)
+            {
+                if (clientListenerThread == null &&
+                    localSocket != null)
+                {
+                    isClientListenerThreadRunning = true;
+                    clientListenerThread = new Thread(ClientListenerThreadFunction);
+                    clientListenerThread.IsBackground = true;
+                    clientListenerThread.Start();
+                }
+            }
         }
 
         public override void StopServer()
         {
-            throw new NotImplementedException();
+            lock (this)
+            {
+                if (clientListenerThread != null)
+                {
+                    isClientListenerThreadRunning = false;
+                    clientListenerThread.Join();
+                }
+            }
+        }
+
+        private void ClientListenerThreadFunction()
+        {
+            while (isClientListenerThreadRunning)
+            {
+
+            }
+
+            isClientListenerThreadRunning = false;
+            clientListenerThread = null;
+
+            status = ServerStatus.Stopped;
         }
     }
 }

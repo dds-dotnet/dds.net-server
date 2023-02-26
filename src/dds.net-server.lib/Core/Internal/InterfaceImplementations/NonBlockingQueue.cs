216082f0566175e04cf50645d55e0b76894f7a39
usama <mahmoodify@gmail.com>
2023-02-26 18:04:02 +0500

    impl blank

---
+++ src/dds.net-server.lib/Core/Internal/InterfaceImplementations/NonBlockingQueue.cs
@@ -0,0 +1,29 @@
+﻿using DDS.Net.Server.Core.Internal.Interfaces;
+
+namespace DDS.Net.Server.Core.Internal.InterfaceImplementations
+{
+    internal class NonBlockingQueue<T> : INonBlockingDataOutputQueueEnd<T>, INonBlockingDataInputQueueEnd<T>
+    {
+        public event EventHandler<T>? InputDataAvailable;
+
+        public bool CanDequeueData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanEnqueueData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DequeueData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnqueueData(T data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

a2082f9a4ca459f5552b2c7168c2f60bb1790160
usama <mahmoodify@gmail.com>
2023-02-26 17:41:18 +0500

    cleaningup and adding methods for iface

--- src/dds.net-server.lib/Core/Internal/Interfaces/IThreadedDataIO.cs
+++ src/dds.net-server.lib/Core/Internal/Interfaces/IThreadedDataIO.cs
@@ -1,14 +1,15 @@
 ﻿using DDS.Net.Server.Core.Internal.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DDS.Net.Server.Core.Internal.Interfaces
 {
     internal interface IThreadedDataIO
     {
         event EventHandler<ThreadedDataIOStatus>? ThreadedDataIOStatusChanged;
+
+        void SetOutputDataQueueEnd(INonBlockingDataOutputQueue outputQueueEnd);
+        void SetInputDataQueueEnd(INonBlockingDataInputQueue inputQueueEnd);
+
+        INonBlockingDataOutputQueue GetOutputDataQueueEnd();
+        INonBlockingDataInputQueue GetInputDataQueueEnd();
     }
 }

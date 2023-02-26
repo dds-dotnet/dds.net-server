eb0c123ce2b75192a8ca64d7799c1a0ad4e57394
usama <mahmoodify@gmail.com>
2023-02-26 17:11:55 +0500

    adding data to client entity

---
+++ src/dds.net-server.lib/Core/Internal/Entities/DataPacketToClient.cs
@@ -0,0 +1,14 @@
+﻿namespace DDS.Net.Server.Core.Internal.Entities
+{
+    internal class DataPacketToClient
+    {
+        public string ClientRef { get; }
+        public byte[] Data { get; set; }
+
+        public DataPacketToClient(string client, byte[] data)
+        {
+            ClientRef = client;
+            Data = data;
+        }
+    }
+}

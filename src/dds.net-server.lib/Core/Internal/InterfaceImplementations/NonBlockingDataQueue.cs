86549522d762629272fcaa0c0de71fde3bce66d2
usama <mahmoodify@gmail.com>
2023-02-26 17:01:47 +0500

    adding blank impl for queue

---
+++ src/dds.net-server.lib/Core/Internal/InterfaceImplementations/NonBlockingDataQueue.cs
@@ -0,0 +1,13 @@
+﻿using DDS.Net.Server.Core.Internal.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDS.Net.Server.Core.Internal.InterfaceImplementations
+{
+    internal class NonBlockingDataQueue : INonBlockingDataQueue
+    {
+    }
+}

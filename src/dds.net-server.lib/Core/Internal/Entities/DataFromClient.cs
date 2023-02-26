4232074d491b88dd5b23f68a38828d8dd872d19d
usama <mahmoodify@gmail.com>
2023-02-26 17:13:37 +0500

    adding data packet

---
+++ src/dds.net-server.lib/Core/Internal/Entities/DataFromClient.cs
@@ -0,0 +1,14 @@
+﻿namespace DDS.Net.Server.Core.Internal.Entities
+{
+    internal class DataFromClient
+    {
+        public string ClientRef { get; }
+        public byte[] Data { get; set; }
+
+        public DataFromClient(string client, byte[] data)
+        {
+            ClientRef = client;
+            Data = data;
+        }
+    }
+}

3d2531cd3d7d5d477b23219cf8283fa24ac2a1ee
usama <mahmoodify@gmail.com>
2023-02-28 03:54:09 +0500

    impl ctor data elements

--- src/dds.net-server.lib/Core/Internal/Base/SinglePipedThread.cs
+++ src/dds.net-server.lib/Core/Internal/Base/SinglePipedThread.cs
@@ -1,4 +1,6 @@
-﻿using DDS.Net.Server.Core.Internal.Interfaces;
+﻿using DDS.Net.Server.Core.Internal.Entities;
+using DDS.Net.Server.Core.Internal.InterfaceImplementations;
+using DDS.Net.Server.Core.Internal.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,27 @@ namespace DDS.Net.Server.Core.Internal.Base
 
         public ISyncDataInputQueueEnd<T_Commands> QueuedCommands { get; private set; }
         public ISyncDataOutputQueueEnd<T_Responses> QueuedResponses { get; private set; }
+
+        protected readonly SyncQueue<T_Input> dataInputQueue;
+        protected readonly SyncQueue<T_Output> dataOutputQueue;
+
+        protected readonly SyncQueueStruct<T_Commands> commandsInputQueue;
+        protected readonly SyncQueueStruct<T_Responses> responsesOutputQueue;
+
+        protected SinglePipedThread(int inputQueueSize, int outputQueueSize, int commandsQueueSize, int responsesQueueSize)
+        {
+            dataInputQueue = new SyncQueue<T_Input>(inputQueueSize);
+            dataOutputQueue = new SyncQueue<T_Output>(outputQueueSize);
+
+            commandsInputQueue = new SyncQueueStruct<T_Commands>(commandsQueueSize);
+            responsesOutputQueue = new SyncQueueStruct<T_Responses>(responsesQueueSize);
+
+            QueuedInput = dataInputQueue;
+            QueuedOutput = dataOutputQueue;
+
+            QueuedCommands = commandsInputQueue;
+            QueuedResponses = responsesOutputQueue;
+        }
 
         public void Dispose()
         {

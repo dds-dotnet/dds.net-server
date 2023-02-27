c13c90ed7f16d46d8de34cb2f87ad9c39dc9925e
usama <mahmoodify@gmail.com>
2023-02-27 13:42:49 +0500

    adding internal elements to hold provided config

--- src/dds.net-server.lib/Core/Internal/InterfaceImplementations/ThreadedNetworkIO.cs
+++ src/dds.net-server.lib/Core/Internal/InterfaceImplementations/ThreadedNetworkIO.cs
@@ -11,18 +11,47 @@ namespace DDS.Net.Server.Core.Internal.InterfaceImplementations
 {
     internal class ThreadedNetworkIO : IThreadedDataIO<DataToClient, DataFromClient>
     {
+        private readonly INonBlockingDataInputQueueEnd<DataToClient> inputQueue;
+        private readonly INonBlockingDataOutputQueueEnd<DataFromClient> outputQueue;
+
+        private readonly ILogger logger;
+
+        private readonly string listeningIPv4Address;
+
+        private readonly bool tcpEnable;
+        private readonly ushort tcpPort;
+        private readonly int tcpMaxClients;
+
+        private readonly bool udpEnable;
+        private readonly ushort udpPort;
+        private readonly int udpMaxClients;
+
         public event EventHandler<ThreadedDataIOStatus>? ThreadedDataIOStatusChanged;
 
         public ThreadedNetworkIO(
             INonBlockingDataInputQueueEnd<DataToClient> inputQueue,
             INonBlockingDataOutputQueueEnd<DataFromClient> outputQueue,
+
             ILogger logger,
 
             string listeningIPv4Address,
             bool tcpEnable, ushort tcpPort, int tcpMaxClients,
             bool udpEnable, ushort udpPort, int udpMaxClients)
         {
-            
+            this.inputQueue = inputQueue;
+            this.outputQueue = outputQueue;
+
+            this.logger = logger;
+
+            this.listeningIPv4Address = listeningIPv4Address;
+
+            this.tcpEnable = tcpEnable;
+            this.tcpPort = tcpPort;
+            this.tcpMaxClients = tcpMaxClients;
+
+            this.udpEnable = udpEnable;
+            this.udpPort = udpPort;
+            this.udpMaxClients = udpMaxClients;
         }
 
         public INonBlockingDataInputQueueEnd<DataToClient> GetInputDataQueueEnd()

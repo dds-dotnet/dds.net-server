53815ee383ea122cccf3e3ade96220aeef35a913
usama <mahmoodify@gmail.com>
2023-02-28 04:59:16 +0500

    adding thread starting

--- src/dds.net-server.lib/Core/Internal/Base/PipedThreadBase.cs
+++ src/dds.net-server.lib/Core/Internal/Base/PipedThreadBase.cs
@@ -14,13 +14,23 @@ namespace DDS.Net.Server.Core.Internal.Base
         protected readonly SyncQueueStruct<T_Commands> commandsQueue;
         protected readonly SyncQueueStruct<T_Responses> responsesQueue;
 
-        protected PipedThreadBase(int commandsQueueSize, int responsesQueueSize)
+        protected PipedThreadBase(int commandsQueueSize, int responsesQueueSize, bool startThread = true)
         {
             commandsQueue = new SyncQueueStruct<T_Commands>(commandsQueueSize);
             responsesQueue = new SyncQueueStruct<T_Responses>(responsesQueueSize);
 
             Commands = commandsQueue;
             Responses = responsesQueue;
+
+            if (startThread)
+            {
+                StartThread();
+            }
+        }
+
+        protected virtual void StartThread()
+        {
+
         }
 
         protected abstract void DoWork();

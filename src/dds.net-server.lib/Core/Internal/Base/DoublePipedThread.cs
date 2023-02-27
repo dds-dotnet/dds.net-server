6ddbb38479e468c07eafdfccf604c7086ebd8490
usama <mahmoodify@gmail.com>
2023-02-28 03:59:16 +0500

    adding ctor

--- src/dds.net-server.lib/Core/Internal/Base/DoublePipedThread.cs
+++ src/dds.net-server.lib/Core/Internal/Base/DoublePipedThread.cs
@@ -1,4 +1,5 @@
-﻿using DDS.Net.Server.Core.Internal.Interfaces;
+﻿using DDS.Net.Server.Core.Internal.InterfaceImplementations;
+using DDS.Net.Server.Core.Internal.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,38 @@ namespace DDS.Net.Server.Core.Internal.Base
 
         public ISyncDataInputQueueEnd<T_Commands> QueuedCommands { get; private set; }
         public ISyncDataOutputQueueEnd<T_Responses> QueuedResponses { get; private set; }
+
+        protected readonly SyncQueue<T_Input1> dataInputQueue1;
+        protected readonly SyncQueue<T_Output1> dataOutputQueue1;
+
+        protected readonly SyncQueue<T_Input2> dataInputQueue2;
+        protected readonly SyncQueue<T_Output2> dataOutputQueue2;
+
+        protected readonly SyncQueueStruct<T_Commands> commandsInputQueue;
+        protected readonly SyncQueueStruct<T_Responses> responsesOutputQueue;
+
+        protected DoublePipedThread(int inputQueue1Size, int outputQueue1Size,
+                                    int inputQueue2Size, int outputQueue2Size,
+                                    int commandsQueueSize, int responsesQueueSize)
+        {
+            dataInputQueue1 = new SyncQueue<T_Input1>(inputQueue1Size);
+            dataOutputQueue1 = new SyncQueue<T_Output1>(outputQueue1Size);
+
+            dataInputQueue2 = new SyncQueue<T_Input2>(inputQueue2Size);
+            dataOutputQueue2 = new SyncQueue<T_Output2>(outputQueue2Size);
+
+            commandsInputQueue = new SyncQueueStruct<T_Commands>(commandsQueueSize);
+            responsesOutputQueue = new SyncQueueStruct<T_Responses>(responsesQueueSize);
+
+            QueuedInput1 = dataInputQueue1;
+            QueuedOutput1 = dataOutputQueue1;
+
+            QueuedInput2 = dataInputQueue2;
+            QueuedOutput2 = dataOutputQueue2;
+
+            QueuedCommands = commandsInputQueue;
+            QueuedResponses = responsesOutputQueue;
+        }
 
         public void Dispose()
         {

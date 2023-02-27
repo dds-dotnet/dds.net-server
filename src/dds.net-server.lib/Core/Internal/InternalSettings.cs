d4cf45f226fe1e7ad71c773fc36adb73502cb169
usama <mahmoodify@gmail.com>
2023-02-27 16:58:29 +0500

    renaming queue sizes - more descriptive names

--- src/dds.net-server.lib/Core/Internal/InternalSettings.cs
+++ src/dds.net-server.lib/Core/Internal/InternalSettings.cs
@@ -5,10 +5,10 @@
         public static readonly int NETWORK_DATA_FROM_CLIENTS_QUEUE_SIZE = 1000;
         public static readonly int NETWORK_DATA_TO_CLIENTS_QUEUE_SIZE = 1000;
 
-        public static readonly int TCP_CLIENTS_INPUT_QUEUE_SIZE = 1000;
-        public static readonly int TCP_CLIENTS_OUTPUT_QUEUE_SIZE = 1000;
+        public static readonly int TCP_DATA_FROM_CLIENTS_QUEUE_SIZE = 1000;
+        public static readonly int TCP_DATA_TO_CLIENTS_QUEUE_SIZE = 1000;
 
-        public static readonly int UDP_CLIENTS_INPUT_QUEUE_SIZE = 1000;
-        public static readonly int UDP_CLIENTS_OUTPUT_QUEUE_SIZE = 1000;
+        public static readonly int UDP_DATA_FROM_CLIENTS_QUEUE_SIZE = 1000;
+        public static readonly int UDP_DATA_TO_CLIENTS_QUEUE_SIZE = 1000;
     }
 }

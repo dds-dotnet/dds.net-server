b20b994b2453ab4b5427cd48a6699dc3bdfe4827
usama <mahmoodify@gmail.com>
2023-02-28 03:10:25 +0500

    outlining the class

--- src/dds.net-server.lib/Core/Internal/Base/PipedThread.cs
+++ src/dds.net-server.lib/Core/Internal/Base/PipedThread.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DDS.Net.Server.Core.Internal.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace DDS.Net.Server.Core.Internal.Base
 {
-    internal class PipedThread
+    internal class PipedThread<T_Input, T_Output, T_Commands, T_Responses>
+        : IDisposable
+        where T_Input : class
+        where T_Output : class
+        where T_Commands : struct
+        where T_Responses : struct
     {
+        public ISyncDataInputQueueEnd<T_Input> InputQueueEnd { get; private set; }
+        public ISyncDataOutputQueueEnd<T_Output> OutputQueueEnd { get; private set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

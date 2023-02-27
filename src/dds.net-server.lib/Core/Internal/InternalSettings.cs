5676e23b34c264eee695872a7680c78d39d6c0f6
usama <mahmoodify@gmail.com>
2023-02-27 15:35:37 +0500

    adding queue sizes

--- src/dds.net-server.lib/Core/Internal/InternalSettings.cs
+++ src/dds.net-server.lib/Core/Internal/InternalSettings.cs
@@ -2,5 +2,7 @@
 {
     internal static class InternalSettings
     {
+        public static readonly int NETWORK_CLIENTS_INPUT_QUEUE_SIZE = 1000;
+        public static readonly int NETWORK_CLIENTS_OUTPUT_QUEUE_SIZE = 1000;
     }
 }

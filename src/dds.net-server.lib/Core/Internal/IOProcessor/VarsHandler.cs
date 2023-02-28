86eaaca9cdeb2575c0d39928931c77a7f0b05bae
usama <mahmoodify@gmail.com>
2023-02-28 15:49:44 +0500

    adding generic handler

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/VarsHandler.cs
@@ -0,0 +1,66 @@
+﻿using DDS.Net.Server.Core.Internal.Base.Entities;
+using DDS.Net.Server.Core.Internal.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DDS.Net.Server.Core.Internal.Interfaces;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor
+{
+    internal class VarsHandler
+        : SingleConsumerPipedThread<DataFromClient, DataToClient, VarsHandlerCommands, VarsHandlerStatus>
+    {
+        public VarsHandler(
+                    ISyncQueueReaderEnd<DataFromClient> dataReaderEnd,
+                    ISyncQueueWriterEnd<DataToClient> dataWriterEnd,
+                    int commandsQueueSize,
+                    int responsesQueueSize)
+
+            : base(dataReaderEnd, dataWriterEnd, commandsQueueSize, responsesQueueSize)
+        {
+            
+        }
+
+        public void StartHandler()
+        {
+            StartThread();
+        }
+
+        public void StopHandler()
+        {
+            Exit();
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DoCleanup()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DoInit()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DoWork()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ProcessCommand(VarsHandlerCommands command)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ProcessInput(DataFromClient input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

f731ae8efcac71e9828c3b0b748eef11440b5e9d
usama <mahmoodify@gmail.com>
2023-02-28 15:55:06 +0500

    adding vars handler settings

--- src/dds.net-server.lib/Core/Internal/InternalSettings.cs
+++ src/dds.net-server.lib/Core/Internal/InternalSettings.cs
@@ -7,6 +7,9 @@
         public static readonly int NETWORK_COMMANDS_QUEUE_SIZE = 20;
         public static readonly int NETWORK_RESPONSES_QUEUE_SIZE = 20;
 
+        public static readonly int VARS_HANDLER_COMMANDS_QUEUE_SIZE = 20;
+        public static readonly int VARS_HANDLER_RESPONSES_QUEUE_SIZE = 20;
+
         public static readonly int TCP_DATA_FROM_CLIENTS_QUEUE_SIZE = 1000;
         public static readonly int TCP_DATA_TO_CLIENTS_QUEUE_SIZE = 1000;
 

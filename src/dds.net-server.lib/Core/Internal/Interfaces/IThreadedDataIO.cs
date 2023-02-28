c19490ae71b8c6dba31e83887f0199f512603a66
usama <mahmoodify@gmail.com>
2023-02-28 05:31:00 +0500

    readdition

---
+++ src/dds.net-server.lib/Core/Internal/Interfaces/IThreadedDataIO.cs
@@ -0,0 +1,18 @@
+﻿using DDS.Net.Server.Core.Internal.Entities;
+
+namespace DDS.Net.Server.Core.Internal.Interfaces
+{
+    internal interface IThreadedDataIO<T_InputQueue, T_OutputQueue>
+    {
+        event EventHandler<ThreadedDataIOStatus>? ThreadedDataIOStatusChanged;
+
+        void StartIO();
+        void StopIO();
+
+        void SetOutputDataQueueEnd(ISyncDataOutputQueueEnd<DataFromClient> outputQueueEnd);
+        void SetInputDataQueueEnd(ISyncDataInputQueueEnd<DataToClient> inputQueueEnd);
+
+        ISyncDataOutputQueueEnd<T_OutputQueue> GetOutputDataQueueEnd();
+        ISyncDataInputQueueEnd<T_InputQueue> GetInputDataQueueEnd();
+    }
+}

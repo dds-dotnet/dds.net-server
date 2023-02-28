e31caa7c496b9bdba2bb2e7c6d0ba63ecbdb800d
usama <mahmoodify@gmail.com>
2023-02-28 06:04:46 +0500

    overriding thread function

--- src/dds.net-server.lib/Core/Internal/Base/DoublePipedThread.cs
+++ src/dds.net-server.lib/Core/Internal/Base/DoublePipedThread.cs
@@ -39,7 +39,24 @@ namespace DDS.Net.Server.Core.Internal.Base
 
             if (startThread)
             {
-                StartThread();
+                StartThread(() =>
+                {
+                    DoInit();
+
+                    while (_isThreadRunning)
+                    {
+                        if (_isThreadRunning) DoWork();
+                        if (_isThreadRunning && commandsQueue.CanDequeue()) CheckCommands();
+                        if (_isThreadRunning) DoWork();
+                        if (_isThreadRunning && (inputQueue.CanDequeue() || inputQueue2.CanDequeue())) CheckInputs();
+                        if (_isThreadRunning) DoWork();
+                        if (_isThreadRunning && (outputQueue.CanEnqueue() || outputQueue2.CanEnqueue())) GenerateOutputs();
+
+                        Thread.Yield();
+                    }
+
+                    DoCleanup();
+                });
             }
         }
     }

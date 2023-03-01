92b63a2b8c8db79657b1d0d29401877799c0a76a
usama <mahmoodify@gmail.com>
2023-03-01 20:56:24 +0500

    adding var type prefixes for var names

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/VarTypePrefix.cs
@@ -0,0 +1,28 @@
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.Types;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Helpers
+{
+    internal static class VarTypePrefix
+    {
+        public static string VarNamePrefix(this VarType varType)
+        {
+            switch (varType)
+            {
+                case VarType.String: return "_str_";
+                case VarType.Boolean: return "_bool_";
+                case VarType.Byte: return "_byte_";
+                case VarType.Word: return "_word_";
+                case VarType.DWord: return "_dword_";
+                case VarType.QWord: return "_qword_";
+                case VarType.UnsignedByte: return "_ubyte_";
+                case VarType.UnsignedWord: return "_uword_";
+                case VarType.UnsignedDWord: return "_udword_";
+                case VarType.UnsignedQWord: return "_uqword_";
+                case VarType.Single: return "_sngl_";
+                case VarType.Double: return "_dbl_";
+            }
+
+            return $"_{varType.ToString()}_";
+        }
+    }
+}

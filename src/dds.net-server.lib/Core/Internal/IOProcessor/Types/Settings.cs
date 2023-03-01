5da5203858c9e047dbc532ecff2810bcb5d7aff5
usama <mahmoodify@gmail.com>
2023-03-01 14:30:34 +0500

    renaming to settings

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Settings.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Settings.cs
@@ -1,6 +1,6 @@
 ﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
 {
-    internal class Constants
+    internal class Settings
     {
         public static readonly int BASE_TIME_SLOT_MS = 25;
     }

b2e2546a4966a1e8060cdec9cc593ac110360eae
usama <mahmoodify@gmail.com>
2023-03-01 20:44:47 +0500

    adding primitive data types

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/VarType.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/VarType.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
+﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
 {
     internal enum VarType
     {
+        String = 0,
+        Boolean,
+
+        Byte,          // 1-Byte Signed Integer
+        Word,          // 2-Byte Signed Integer
+        DWord,         // 4-Byte Signed Integer
+        QWord,         // 8-Byte Signed Integer
+
+        UnsignedByte,  // 1-Byte Unsigned Integer
+        UnsignedWord,  // 2-Byte Unsigned Integer
+        UnsignedDWord, // 4-Byte Unsigned Integer
+        UnsignedQWord, // 8-Byte Unsigned Integer
+
+        Single,        // 4-Byte Floating-point
+        Double,        // 8-Byte Floating-point
     }
 }

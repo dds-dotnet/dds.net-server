bf40a3b7b2ce537608f579d31e910943933b3c6d
usama <mahmoodify@gmail.com>
2023-03-01 22:05:18 +0500

    adding readbool

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/PrimitiveEncoder.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/PrimitiveEncoder.cs
@@ -2,6 +2,21 @@
 {
     internal static class PrimitiveEncoder
     {
+        public static bool ReadBoolean(this byte[] data, ref int offset)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (offset < 0 || offset >= data.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            }
+
+            return data[offset++] != 0;
+        }
+
         public static sbyte ReadByte(this byte[] data, ref int offset)
         {
             if (data == null)

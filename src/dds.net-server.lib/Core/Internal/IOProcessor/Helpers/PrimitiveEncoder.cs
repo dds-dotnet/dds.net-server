cf697baabf7e6032c94b4dae110b24e1f2876e4e
usama <mahmoodify@gmail.com>
2023-03-02 00:18:59 +0500

    adding writing of dword

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/PrimitiveEncoder.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/PrimitiveEncoder.cs
@@ -201,6 +201,23 @@ namespace DDS.Net.Server.Core.Internal.IOProcessor.Helpers
 
             return value;
         }
+        public static void WriteDWord(this byte[] data, ref int offset, int value)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (offset < 0 || offset + 3 >= data.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            }
+
+            data[offset++] = (byte)((value >> 24) & 0x0ff);
+            data[offset++] = (byte)((value >> 16) & 0x0ff);
+            data[offset++] = (byte)((value >>  8) & 0x0ff);
+            data[offset++] = (byte)((value >>  0) & 0x0ff);
+        }
 
         public static long ReadQWord(this byte[] data, ref int offset)
         {

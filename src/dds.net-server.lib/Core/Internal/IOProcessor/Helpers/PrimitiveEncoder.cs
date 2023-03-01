874df4f8504829e0280c40884d7c3656b617e55e
usama <mahmoodify@gmail.com>
2023-03-01 22:02:30 +0500

    adding sbyte reader

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/PrimitiveEncoder.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/PrimitiveEncoder.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DDS.Net.Server.Core.Internal.IOProcessor.Helpers
+﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Helpers
 {
-    internal class PrimitiveEncoder
+    internal static class PrimitiveEncoder
     {
+        public static sbyte ReadByte(this byte[] data, ref int offset)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (offset < 0 || offset >= data.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            }
+
+            return (sbyte)data[offset++];
+        }
     }
 }

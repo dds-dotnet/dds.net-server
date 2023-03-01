8b7dad4a2ef38b264e8835d75888278eaaa041b8
usama <mahmoodify@gmail.com>
2023-03-01 22:19:40 +0500

    adding reading of string

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/PrimitiveEncoder.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/PrimitiveEncoder.cs
@@ -1,7 +1,36 @@
-﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Helpers
+﻿using System.Text;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Helpers
 {
     internal static class PrimitiveEncoder
     {
+        public static string ReadString(this byte[] data, ref int offset)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (offset < 0 || offset + 1 >= data.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            }
+
+            int length = data[offset++];
+            length = (length << 8) | data[offset++];
+
+            if (offset + length > data.Length)
+            {
+                throw new Exception($"String should be {length} bytes long " +
+                                    $"but {offset + length - data.Length} bytes are available");
+            }
+
+            string retval = Encoding.Unicode.GetString(data, offset, length);
+            offset += length;
+
+            return retval;
+        }
+
         public static bool ReadBoolean(this byte[] data, ref int offset)
         {
             if (data == null)

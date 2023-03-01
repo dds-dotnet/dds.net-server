474a8c8b87017762bb451dae9f4e35e95e773ac9
usama <mahmoodify@gmail.com>
2023-03-01 14:36:22 +0500

    adding help

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Periodicity.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Periodicity.cs
@@ -2,11 +2,29 @@
 {
     internal enum Periodicity
     {
+        /// <summary>
+        /// Updates only when changed
+        /// </summary>
         OnChange = 0,
+        /// <summary>
+        /// Updates every { Settings.BASE_TIME_SLOT_MS * 1 } milliseconds
+        /// </summary>
         Highest,
+        /// <summary>
+        /// Updates every { Settings.BASE_TIME_SLOT_MS * 2 } milliseconds
+        /// </summary>
         High,
+        /// <summary>
+        /// Updates every { Settings.BASE_TIME_SLOT_MS * 4 } milliseconds
+        /// </summary>
         Normal,
+        /// <summary>
+        /// Updates every { Settings.BASE_TIME_SLOT_MS * 8 } milliseconds
+        /// </summary>
         Low,
+        /// <summary>
+        /// Updates every { Settings.BASE_TIME_SLOT_MS * 16 } milliseconds
+        /// </summary>
         Lowest
     }
 }

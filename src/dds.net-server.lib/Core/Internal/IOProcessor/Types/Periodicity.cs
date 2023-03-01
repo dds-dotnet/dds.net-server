d440c4003843c24d5394fffd0deec4e16bc019c8
usama <mahmoodify@gmail.com>
2023-03-01 14:25:52 +0500

    adding periodicity definition - representing lanes of update

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Periodicity.cs
@@ -0,0 +1,12 @@
+﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
+{
+    internal enum Periodicity
+    {
+        OnChange = 0,
+        Highest,
+        High,
+        Normal,
+        Low,
+        Lowest
+    }
+}

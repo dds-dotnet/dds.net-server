0776583f0d2aeeebbc3e1822de4c935fb6adb1ec
usama <mahmoodify@gmail.com>
2023-03-01 20:33:05 +0500

    adding settings for variables

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Settings.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Settings.cs
@@ -3,5 +3,9 @@
     internal class Settings
     {
         public static readonly int BASE_TIME_SLOT_MS = 50;
+
+        public static readonly int MAX_VARIABLE_NAME_LENGTH = 127;
+        public static readonly int MAX_STRING_VARIABLES_PER_MESSAGE = 10;
+        public static readonly int MAX_PRIMITIVE_VARIABLES_PER_MESSAGE = 100;
     }
 }

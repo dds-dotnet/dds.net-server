fe3579ca1eb1bf57a85d953331148d920b8fd075
usama <mahmoodify@gmail.com>
2023-03-02 23:20:23 +0500

    changing initialization

--- src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-Database.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-Database.cs
@@ -1,19 +1,28 @@
 ﻿using DDS.Net.Server.Core.Internal.Base;
 using DDS.Net.Server.Core.Internal.Base.Entities;
+using DDS.Net.Server.Core.Internal.IOProcessor.Types;
 
 namespace DDS.Net.Server.Core.Internal.IOProcessor
 {
     internal partial class VariablesDatabase
         : SinglePipedConsumer<DataFromClient, DataToClient, VarsDbCommand, VarsDbStatus>
     {
+        private Mutex _primitivesDatabaseMutex = new();
+        private Dictionary<string, VariableDefinition> _primitivesDatabaseDictionary = new();
+
         private void InitializeDatabase()
         {
-
+            lock (_primitivesDatabaseMutex)
+            {
+            }
         }
 
         private void ClearDatabase()
         {
-
+            lock (_primitivesDatabaseMutex)
+            {
+                _primitivesDatabaseDictionary.Clear();
+            }
         }
     }
 }

39d42bdd0a7a8872859d433f2e61b78a50320e16
usama <mahmoodify@gmail.com>
2023-03-02 17:59:54 +0500

    adding byte swapping

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Helpers/ByteSwap.cs
@@ -0,0 +1,17 @@
+﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Helpers
+{
+    internal static class ByteSwap
+    {
+        public static byte[] ReverseArray(this byte[] data)
+        {
+            for (int i = 0; i < data.Length / 2; i++)
+            {
+                byte temp = data[i];
+                data[i] = data[data.Length - i - 1];
+                data[data.Length - i - 1] = temp;
+            }
+
+            return data;
+        }
+    }
+}

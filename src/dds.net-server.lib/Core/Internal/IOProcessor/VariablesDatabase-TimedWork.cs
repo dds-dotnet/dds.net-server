6d2f03ec63e22ad29fbb5d43a186143fba29385b
usama <mahmoodify@gmail.com>
2023-03-02 22:16:27 +0500

    outlining the timed work

--- src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-TimedWork.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-TimedWork.cs
@@ -1,24 +1,63 @@
 ﻿using DDS.Net.Server.Core.Internal.Base;
 using DDS.Net.Server.Core.Internal.Base.Entities;
+using DDS.Net.Server.Core.Internal.IOProcessor.Types;
 
 namespace DDS.Net.Server.Core.Internal.IOProcessor
 {
     internal partial class VariablesDatabase
         : SinglePipedConsumer<DataFromClient, DataToClient, VarsDbCommand, VarsDbStatus>
     {
+        private Timer _timer;
+        private int _timedWorkCounter = 0;
+        private volatile bool _isTimedWorkRunning = false;
+
         private void StartTimedWork()
         {
+            if (_isTimedWorkRunning == false)
+            {
+                _timedWorkCounter = 0;
+                _isTimedWorkRunning = true;
 
+                try
+                {
+                    _timer = new Timer(TimerCallback);
+                    _timer.Change(Settings.BASE_TIME_SLOT_MS, Timeout.Infinite);
+                }
+                catch (Exception ex)
+                {
+                    _timer = null!;
+                    logger.Error($"Variables Database - Timed work cannot be executed: {ex.Message}");
+                }
+            }
         }
 
         private void StopTimedWork()
         {
+            _isTimedWorkRunning = false;
 
+            if (_timer != null)
+            {
+                _timer.Change(Timeout.Infinite, Timeout.Infinite);
+                _timer.Dispose();
+            }
         }
 
-        private void TimeWork()
+        private void TimerCallback(object? o)
         {
+            DoTimedWork();
 
+            if (_isTimedWorkRunning)
+            {
+                try
+                {
+                    _timer.Change(Settings.BASE_TIME_SLOT_MS, Timeout.Infinite);
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void DoTimedWork()
+        {
         }
     }
 }

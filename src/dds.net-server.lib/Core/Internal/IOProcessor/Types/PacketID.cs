8aac5f465d5af433dbc1fd64bca88ad0719be047
usama <mahmoodify@gmail.com>
2023-03-02 19:38:16 +0500

    adding comments

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/PacketID.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/PacketID.cs
@@ -2,11 +2,29 @@
 {
     internal enum PacketID
     {
+        /// <summary>
+        /// Information exchange
+        /// </summary>
         HandShake = 0,
+        /// <summary>
+        /// Registering variables with the server
+        /// </summary>
         VariableRegistration = 1,
+        /// <summary>
+        /// Updating primitive variable values at the server
+        /// </summary>
         PrimitivesUpdateAtServer = 2,
+        /// <summary>
+        /// Updating primitive variable values at the client
+        /// </summary>
         PrimitivesUpdateFromServer = 3,
 
+
+
+
+        /// <summary>
+        /// Unknown packet
+        /// </summary>
         UNKNOWN
     }
 }

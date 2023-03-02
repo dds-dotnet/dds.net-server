83c8277e3810a06049782d8c7b200d2f3eb12fee
usama <mahmoodify@gmail.com>
2023-03-02 21:45:48 +0500

    adding comments

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/VariableType.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/VariableType.cs
@@ -2,22 +2,58 @@
 {
     internal enum VariableType
     {
+        /// <summary>
+        /// Represents a string of characters
+        /// </summary>
         String = 0,
+        /// <summary>
+        /// Represents a boolean (True or False)
+        /// </summary>
         Boolean = 1,
+        /// <summary>
+        /// Represents a 1-byte Signed Integer
+        /// </summary>
+        Byte = 2,
+        /// <summary>
+        /// Represents a 2-byte Signed Integer
+        /// </summary>
+        Word = 3,
+        /// <summary>
+        /// Represents a 4-byte Signed Integer
+        /// </summary>
+        DWord = 4,
+        /// <summary>
+        /// Represents a 8-byte Signed Integer
+        /// </summary>
+        QWord = 5,
+        /// <summary>
+        /// Represents a 1-byte Unsigned Integer
+        /// </summary>
+        UnsignedByte = 6,
+        /// <summary>
+        /// Represents a 2-byte Unsigned Integer
+        /// </summary>
+        UnsignedWord = 7,
+        /// <summary>
+        /// Represents a 4-byte Unsigned Integer
+        /// </summary>
+        UnsignedDWord = 8,
+        /// <summary>
+        /// Represents a 8-byte Unsigned Integer
+        /// </summary>
+        UnsignedQWord = 9,
+        /// <summary>
+        /// Represents a single precision 4-byte Floating-point number
+        /// </summary>
+        Single = 10,
+        /// <summary>
+        /// Represents a double precision 8-byte Floating-point number
+        /// </summary>
+        Double = 11,
 
-        Byte = 2,          // 1-Byte Signed Integer
-        Word = 3,          // 2-Byte Signed Integer
-        DWord = 4,         // 4-Byte Signed Integer
-        QWord = 5,         // 8-Byte Signed Integer
-
-        UnsignedByte = 6,  // 1-Byte Unsigned Integer
-        UnsignedWord = 7,  // 2-Byte Unsigned Integer
-        UnsignedDWord = 8, // 4-Byte Unsigned Integer
-        UnsignedQWord = 9, // 8-Byte Unsigned Integer
-
-        Single = 10,        // 4-Byte Floating-point
-        Double = 11,        // 8-Byte Floating-point
-
+        /// <summary>
+        /// Unknown type
+        /// </summary>
         UNKNOWN
     }
 }

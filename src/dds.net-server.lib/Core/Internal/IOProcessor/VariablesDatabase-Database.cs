1a1e7563b747ef7f046f915b224a3bd484cba7b1
usama <mahmoodify@gmail.com>
2023-03-02 22:18:58 +0500

    placeholder for db

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-Database.cs
@@ -0,0 +1,10 @@
+﻿using DDS.Net.Server.Core.Internal.Base;
+using DDS.Net.Server.Core.Internal.Base.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor
+{
+    internal partial class VariablesDatabase
+        : SinglePipedConsumer<DataFromClient, DataToClient, VarsDbCommand, VarsDbStatus>
+    {
+    }
+}

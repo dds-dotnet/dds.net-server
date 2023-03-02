a507dde0f0c008b6ccc1b32491ac9854da8aa3f4
usama <mahmoodify@gmail.com>
2023-03-02 19:35:35 +0500

    adding packet ID

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/PacketID.cs
@@ -0,0 +1,12 @@
+﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
+{
+    internal enum PacketID
+    {
+        HandShake = 0,
+        VariableRegistration = 1,
+        PrimitivesUpdateAtServer = 2,
+        PrimitivesUpdateFromServer = 3,
+
+        UNKNOWN
+    }
+}

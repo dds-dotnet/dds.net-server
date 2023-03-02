5d564ea08433bf5821807d86bbeb6ab662a1b173
usama <mahmoodify@gmail.com>
2023-03-02 23:29:00 +0500

    adding subscriber

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/VariableSubscriber.cs
@@ -0,0 +1,8 @@
+﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
+{
+    internal class VariableSubscriber
+    {
+        public string ClientRef { get; set; } = "";
+        public Periodicity Periodicity { get; set; }
+    }
+}

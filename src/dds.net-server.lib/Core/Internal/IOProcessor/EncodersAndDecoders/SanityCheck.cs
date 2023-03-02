0e1a22d7a8f5edd76d14da339021bd64e0ae2230
usama <mahmoodify@gmail.com>
2023-03-02 16:50:05 +0500

    adding extension method for checking required size

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/EncodersAndDecoders/SanityCheck.cs
@@ -0,0 +1,20 @@
+﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders
+{
+    internal static class SanityCheck
+    {
+        public static void CheckRequiredSize(this byte[] data, ref int offset, int requiredSize)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (offset < 0 || offset + requiredSize - 1 >= data.Length)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(offset),
+                    $"Array size is {data.Length}, needed data size is {requiredSize} at offset {offset}");
+            }
+        }
+    }
+}

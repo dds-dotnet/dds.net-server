f5d79b586ee4f1dbaacd5f1a9d48cfc43c942e9a
usama <mahmoodify@gmail.com>
2023-03-03 16:23:28 +0500

    making it generic

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/VariableDefinition.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/VariableDefinition.cs
@@ -1,12 +1,12 @@
 ﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
 {
-    internal class VariableDefinition
+    internal class VariableDefinition<T>
+        where T : struct
     {
         public VariableType VariableType { get; set; } = VariableType.UNKNOWN;
 
         public ushort ID { get; set; }
-        public string Name { get; set; } = "";
-        public object Value { get; set; } = null!;
+        public T Value { get; set; }
         
         public List<VariableProvider> Providers { get; set; } = new();
         public List<VariableSubscriber> Subscribers { get; set; } = new();

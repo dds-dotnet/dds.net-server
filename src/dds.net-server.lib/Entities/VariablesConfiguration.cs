3106b12e065870f2de464ddcc5db855ee9b46525
usama <mahmoodify@gmail.com>
2023-03-05 14:19:05 +0500

    adding var settings

--- src/dds.net-server.lib/Entities/VariablesConfiguration.cs
+++ src/dds.net-server.lib/Entities/VariablesConfiguration.cs
@@ -2,5 +2,20 @@
 {
     public class VariablesConfiguration
     {
+        public List<VariableSettings> Settings { get; private set; } = new();
+
+        public void AddVariableSettings(VariableSettings settings)
+        {
+            foreach (var setting in Settings)
+            {
+                if (setting.VariableName == settings.VariableName)
+                {
+                    Settings.Remove(setting);
+                    break;
+                }
+            }
+
+            Settings.Add(settings);
+        }
     }
 }

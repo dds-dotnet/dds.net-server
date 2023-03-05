214dd883dcecab20c8e7d350f38f7e8e3889a7cf
usama <mahmoodify@gmail.com>
2023-03-05 23:21:41 +0500

    cleaning up and making variable a baseclass

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/CompoundVariable.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/CompoundVariable.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
+﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
 {
-    internal class CompoundVariable
+    internal class CompoundVariable : Variable
     {
+        public CompoundVariable(ushort id) : base(id)
+        {
+
+        }
     }
 }

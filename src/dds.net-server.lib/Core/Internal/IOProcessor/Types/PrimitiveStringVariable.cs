999256667febf63a5f8de9c880e55017c7e357dd
usama <mahmoodify@gmail.com>
2023-03-05 22:54:42 +0500

    adding separate string variable

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/PrimitiveStringVariable.cs
@@ -0,0 +1,16 @@
+﻿using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
+{
+    internal class PrimitiveStringVariable
+    {
+        public PrimitiveType PrimitiveType { get; private set; } = PrimitiveType.String;
+
+        public string Value { get; set; }
+
+        public PrimitiveStringVariable()
+        {
+            Value = string.Empty;
+        }
+    }
+}

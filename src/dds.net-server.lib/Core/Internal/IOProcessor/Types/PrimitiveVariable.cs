bdaa211bc8cc4e75779846e3ee5df8739d4bb842
usama <mahmoodify@gmail.com>
2023-03-05 23:09:35 +0500

    adding initial types

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/PrimitiveVariable.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/PrimitiveVariable.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DDS.Net.Server.Entities;
 
 namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
 {
-    internal class PrimitiveVariable
+    internal class PrimitiveVariable<T>
+        where T : struct
     {
+        public PrimitiveType PrimitiveType { get; private set; }
+
+        public T Value { get; set; }
+
+        public PrimitiveVariable()
+        {
+            if (typeof(T) == typeof(bool))         { PrimitiveType = PrimitiveType.Boolean;       }
+            else if (typeof(T) == typeof(sbyte))   { PrimitiveType = PrimitiveType.Byte;          }
+            else if (typeof(T) == typeof(short))   { PrimitiveType = PrimitiveType.Word;          }
+            else if (typeof(T) == typeof(int))     { PrimitiveType = PrimitiveType.DWord;         }
+            else if (typeof(T) == typeof(long))    { PrimitiveType = PrimitiveType.QWord;         }
+            else if (typeof(T) == typeof(byte))    { PrimitiveType = PrimitiveType.UnsignedByte;  }
+            else if (typeof(T) == typeof(ushort))  { PrimitiveType = PrimitiveType.UnsignedWord;  }
+            else if (typeof(T) == typeof(uint))    { PrimitiveType = PrimitiveType.UnsignedDWord; }
+            else if (typeof(T) == typeof(ulong))   { PrimitiveType = PrimitiveType.UnsignedQWord; }
+            else if (typeof(T) == typeof(float))   { PrimitiveType = PrimitiveType.Single;        }
+            else if (typeof(T) == typeof(double))  { PrimitiveType = PrimitiveType.Double;        }
+            else                                   { PrimitiveType = PrimitiveType.UNKNOWN;       }
+        }
     }
 }

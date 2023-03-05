413031fcb705229de3c6ee9a0c7564ffdd737aba
usama <mahmoodify@gmail.com>
2023-03-05 23:19:49 +0500

    initializing different types as supported

--- src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-Database.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-Database.cs
@@ -11,14 +11,39 @@ namespace DDS.Net.Server.Core.Internal.IOProcessor
     {
         private Mutex _dbMutex = new();
         private Dictionary<string, ushort> _dbNameToId = new();
-        private Dictionary<ushort, Variable> _dbIdToValue = new();
+        private Dictionary<ushort, PrimitiveStringVariable> _dbStrings = new();
+        private Dictionary<ushort, PrimitiveVariable<bool>> _dbBooleans = new();
+        private Dictionary<ushort, PrimitiveVariable<sbyte>> _dbBytes = new();
+        private Dictionary<ushort, PrimitiveVariable<short>> _dbWords = new();
+        private Dictionary<ushort, PrimitiveVariable<int>> _dbDWords = new();
+        private Dictionary<ushort, PrimitiveVariable<long>> _dbQWords = new();
+        private Dictionary<ushort, PrimitiveVariable<byte>> _dbUnsignedBytes = new();
+        private Dictionary<ushort, PrimitiveVariable<ushort>> _dbUnsignedWords = new();
+        private Dictionary<ushort, PrimitiveVariable<uint>> _dbUnsignedDWords = new();
+        private Dictionary<ushort, PrimitiveVariable<ulong>> _dbUnsignedQWords = new();
+        private Dictionary<ushort, PrimitiveVariable<float>> _dbSingles = new();
+        private Dictionary<ushort, PrimitiveVariable<double>> _dbDoubles = new();
+        private Dictionary<ushort, CompoundVariable> _dbCompoundVariables = new();
 
         private void InitializeDatabase()
         {
             lock (_dbMutex)
             {
                 _dbNameToId.Clear();
-                _dbIdToValue.Clear();
+
+                _dbStrings.Clear();
+                _dbBooleans.Clear();
+                _dbBytes.Clear();
+                _dbWords.Clear();
+                _dbDWords.Clear();
+                _dbQWords.Clear();
+                _dbUnsignedBytes.Clear();
+                _dbUnsignedWords.Clear();
+                _dbUnsignedDWords.Clear();
+                _dbUnsignedQWords.Clear();
+                _dbSingles.Clear();
+                _dbDoubles.Clear();
+                _dbCompoundVariables.Clear();
 
                 foreach (VariableSettings settings in variablesConfiguration.Settings)
                 {
@@ -43,7 +68,20 @@ namespace DDS.Net.Server.Core.Internal.IOProcessor
             lock (_dbMutex)
             {
                 _dbNameToId.Clear();
-                _dbIdToValue.Clear();
+
+                _dbStrings.Clear();
+                _dbBooleans.Clear();
+                _dbBytes.Clear();
+                _dbWords.Clear();
+                _dbDWords.Clear();
+                _dbQWords.Clear();
+                _dbUnsignedBytes.Clear();
+                _dbUnsignedWords.Clear();
+                _dbUnsignedDWords.Clear();
+                _dbUnsignedQWords.Clear();
+                _dbSingles.Clear();
+                _dbDoubles.Clear();
+                _dbCompoundVariables.Clear();
             }
         }
     }

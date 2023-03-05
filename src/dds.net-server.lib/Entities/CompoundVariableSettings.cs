f7ab20def7f6d3153615f58674288eb6889080a6
usama <mahmoodify@gmail.com>
2023-03-05 15:57:49 +0500

    adding compound var settings

---
+++ src/dds.net-server.lib/Entities/CompoundVariableSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace DDS.Net.Server.Entities
+{
+    public class CompoundVariableSettings : VariableSettings
+    {
+        public List<string> PrimitiveNames { get; private set; }
+
+        public CompoundVariableSettings(string name, List<string> primitives) : base(name)
+        {
+            PrimitiveNames = primitives;
+        }
+    }
+}

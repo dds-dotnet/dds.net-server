9d230a7545e3d64de36b5399f07e8df88136d937
usama <mahmoodify@gmail.com>
2023-03-05 23:19:22 +0500

    adding ctor and making ID unchangeable

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable.cs
@@ -1,12 +1,15 @@
-﻿using DDS.Net.Server.Entities;
-
-namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
+﻿namespace DDS.Net.Server.Core.Internal.IOProcessor.Types
 {
     internal class Variable
     {
-        public ushort Id { get; set; }
+        public ushort Id { get; private set; }
 
         public List<VariableProvider> Providers { get; set; } = new();
         public List<VariableSubscriber> Subscribers { get; set; } = new();
+
+        public Variable(ushort id)
+        {
+            Id = id;
+        }
     }
 }

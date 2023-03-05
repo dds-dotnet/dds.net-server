51b5da2e3c67e77a6c395648e00d12a5f564e8c4
usama <mahmoodify@gmail.com>
2023-03-05 14:08:07 +0500

    adding placeholder for variable config

---
+++ src/dds.net-server.lib/Entities/VariableConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace DDS.Net.Server.Entities
+{
+    public class VariableConfiguration
+    {
+        public string Name { get; private set; }
+
+        public VariableConfiguration(string name)
+        {
+            Name = name;
+        }
+    }
+}

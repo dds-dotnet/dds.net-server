ae78f6cb2a99cc9ad78508b9c013e6d444230d5e
usama <mahmoodify@gmail.com>
2023-03-05 15:55:54 +0500

    adding primitive variable settings

---
+++ src/dds.net-server.lib/Entities/PrimitiveVariableSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace DDS.Net.Server.Entities
+{
+    public class PrimitiveVariableSettings : VariableSettings
+    {
+        public PrimitiveType PrimitiveType { get; private set; }
+
+        public PrimitiveVariableSettings(string name, PrimitiveType type) : base(name)
+        {
+            PrimitiveType = type;
+        }
+    }
+}

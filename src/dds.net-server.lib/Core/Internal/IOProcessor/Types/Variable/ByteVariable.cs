0e6dd3fa2c9215bb43f92313b72ffabae82d07a3
usama <mahmoodify@gmail.com>
2023-03-06 17:25:37 +0500

    adding byte handling var

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/ByteVariable.cs
@@ -0,0 +1,26 @@
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class ByteVariable<T> : BaseVariable
+    {
+        public PrimitiveType PrimitiveType { get; private set; } = PrimitiveType.Byte;
+
+        public sbyte Value { get; set; }
+
+        public ByteVariable(ushort id, string name) : base(id, name)
+        {
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            return 1;
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            buffer.WriteByte(ref offset, Value);
+        }
+    }
+}

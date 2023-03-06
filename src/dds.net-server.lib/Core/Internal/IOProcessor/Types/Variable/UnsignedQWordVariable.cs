211d1e2b45d124f159d85074c9357cf898b1d128
usama <mahmoodify@gmail.com>
2023-03-06 17:54:42 +0500

    adding handler for ulong

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/UnsignedQWordVariable.cs
@@ -0,0 +1,25 @@
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class UnsignedQWordVariable : BasePrimitive
+    {
+        public ulong Value { get; set; }
+
+        public UnsignedQWordVariable(ushort id, string name) : base(id, name)
+        {
+            PrimitiveType = PrimitiveType.UnsignedQWord;
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            return 8;
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            buffer.WriteUnsignedQWord(ref offset, Value);
+        }
+    }
+}

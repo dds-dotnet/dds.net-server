6d9935f27304162a572bd1fde22d3435028ea11d
usama <mahmoodify@gmail.com>
2023-03-06 17:47:10 +0500

    impl abstract methods

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/StringVariable.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/StringVariable.cs
@@ -1,16 +1,42 @@
-﻿using DDS.Net.Server.Entities;
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Entities;
+using System.Text;
 
 namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
 {
-    internal class StringVariable : BaseVariable
+    internal class StringVariable : BasePrimitive
     {
-        public PrimitiveType PrimitiveType { get; private set; } = PrimitiveType.String;
+        private string _value;
+        private byte[] _bytes;
 
-        public string Value { get; set; }
+        public string Value {
+            get { return _value;  }
+            set
+            {
+                if (value != _value)
+                {
+                    _value = value;
+                    _bytes = Encoding.Unicode.GetBytes(value);
+                }
+            } }
 
         public StringVariable(ushort id, string name) : base(id, name)
         {
+            _value = string.Empty;
+            _bytes = Encoding.Unicode.GetBytes(_value);
+
             Value = string.Empty;
+            PrimitiveType = PrimitiveType.String;
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            return 2 + _bytes.Length;
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            buffer.WriteString(ref offset, Value);
         }
     }
 }

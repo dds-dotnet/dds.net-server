b620753e054212497a804e5b5af0b6c14bdc8825
usama <mahmoodify@gmail.com>
2023-03-06 17:41:03 +0500

    adding float handling var

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/SingleVariable.cs
@@ -0,0 +1,25 @@
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class SingleVariable : BasePrimitive
+    {
+        public float Value { get; set; }
+
+        public SingleVariable(ushort id, string name) : base(id, name)
+        {
+            PrimitiveType = PrimitiveType.Single;
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            return 4;
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            buffer.WriteSingle(ref offset, Value);
+        }
+    }
+}

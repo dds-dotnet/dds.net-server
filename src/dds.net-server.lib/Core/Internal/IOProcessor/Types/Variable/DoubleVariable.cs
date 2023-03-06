a2cab685c31426577b127ab4d66eb395ce53fc8e
usama <mahmoodify@gmail.com>
2023-03-06 17:27:08 +0500

    adding dbl var

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/DoubleVariable.cs
@@ -0,0 +1,26 @@
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class DoubleVariable<T> : BaseVariable
+    {
+        public PrimitiveType PrimitiveType { get; private set; } = PrimitiveType.Double;
+
+        public double Value { get; set; }
+
+        public DoubleVariable(ushort id, string name) : base(id, name)
+        {
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            return 8;
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            buffer.WriteDouble(ref offset, Value);
+        }
+    }
+}

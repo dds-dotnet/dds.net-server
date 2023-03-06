ac5eb4a7f7fdb40cc5447667a692ff86b937199c
usama <mahmoodify@gmail.com>
2023-03-06 17:39:42 +0500

    adding qword

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/QWordVariable.cs
@@ -0,0 +1,25 @@
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class QWordVariable : BasePrimitive
+    {
+        public long Value { get; set; }
+
+        public QWordVariable(ushort id, string name) : base(id, name)
+        {
+            PrimitiveType = PrimitiveType.QWord;
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            return 8;
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            buffer.WriteQWord(ref offset, Value);
+        }
+    }
+}

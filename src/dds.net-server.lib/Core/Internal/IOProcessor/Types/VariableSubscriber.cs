85da62f334acc209ae6da137c358071249fb742e
usama <mahmoodify@gmail.com>
2023-03-06 16:55:51 +0500

    adding equality checks and hashcode

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/VariableSubscriber.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/VariableSubscriber.cs
@@ -2,7 +2,74 @@
 {
     internal class VariableSubscriber
     {
-        public string ClientRef { get; set; } = "";
-        public Periodicity Periodicity { get; set; }
+        public ushort VariableId { get; private set; }
+        public string ClientRef { get; private set; }
+        public Periodicity Periodicity { get; private set; }
+
+        public VariableSubscriber(
+            ushort variableId,
+            string clientRef,
+            Periodicity periodicity)
+        {
+            VariableId = variableId;
+            ClientRef = clientRef;
+            Periodicity = periodicity;
+        }
+
+        public static bool operator ==(VariableSubscriber obj1, VariableSubscriber obj2)
+        {
+            if (obj1 != null! && obj2 != null!)
+            {
+                return
+                    obj1.VariableId  == obj2.VariableId &&
+                    obj1.ClientRef   == obj2.ClientRef &&
+                    obj1.Periodicity == obj2.Periodicity;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(VariableSubscriber obj1, VariableSubscriber obj2)
+        {
+            if (obj1 != null! && obj2 != null!)
+            {
+                bool equals =
+                    obj1.VariableId  == obj2.VariableId &&
+                    obj1.ClientRef   == obj2.ClientRef &&
+                    obj1.Periodicity == obj2.Periodicity;
+
+                return !equals;
+            }
+
+            return true;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            if (obj is VariableSubscriber other)
+            {
+                return this == other;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return
+                VariableId.GetHashCode() ^
+                ClientRef.GetHashCode() ^
+                Periodicity.GetHashCode();
+        }
     }
 }

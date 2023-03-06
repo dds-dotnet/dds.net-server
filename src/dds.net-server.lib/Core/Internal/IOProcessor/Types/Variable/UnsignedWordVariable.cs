b9ddd7bef94289a3404ce90249f575f9a9821307
usama <mahmoodify@gmail.com>
2023-03-06 17:57:03 +0500

    handler for unsigned short variables

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/UnsignedWordVariable.cs
@@ -0,0 +1,25 @@
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class UnsignedWordVariable : BasePrimitive
+    {
+        public ushort Value { get; set; }
+
+        public UnsignedWordVariable(ushort id, string name) : base(id, name)
+        {
+            PrimitiveType = PrimitiveType.UnsignedWord;
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            return 2;
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            buffer.WriteUnsignedWord(ref offset, Value);
+        }
+    }
+}

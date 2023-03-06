ab65772a9bd0aea2b8d6fbf6acdab3c1f560e0bd
usama <mahmoodify@gmail.com>
2023-03-06 17:49:01 +0500

    unknown var placeholder

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/UNKNOWNVariable.cs
@@ -0,0 +1,22 @@
+﻿using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class UNKNOWNVariable : BasePrimitive
+    {
+        public UNKNOWNVariable(ushort id, string name) : base(id, name)
+        {
+            PrimitiveType = PrimitiveType.UNKNOWN;
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            throw new Exception("Unknown variable - size cannot be estimated");
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            throw new Exception("Unknown variable - cannot be written on buffer");
+        }
+    }
+}

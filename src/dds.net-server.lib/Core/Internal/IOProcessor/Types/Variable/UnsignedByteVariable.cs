0660af98502dd4f40f0bda6bd1acd9bd861dc7e2
usama <mahmoodify@gmail.com>
2023-03-06 17:50:51 +0500

    handler for unsigned byte

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/UnsignedByteVariable.cs
@@ -0,0 +1,25 @@
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class UnsignedByteVariable : BasePrimitive
+    {
+        public byte Value { get; set; }
+
+        public UnsignedByteVariable(ushort id, string name) : base(id, name)
+        {
+            PrimitiveType = PrimitiveType.UnsignedByte;
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            return 1;
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            buffer.WriteUnsignedByte(ref offset, Value);
+        }
+    }
+}

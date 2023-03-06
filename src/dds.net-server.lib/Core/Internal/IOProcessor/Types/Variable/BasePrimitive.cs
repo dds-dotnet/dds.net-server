0b128e9f4a13f12c2bd5ce8c7a3def8786b24125
usama <mahmoodify@gmail.com>
2023-03-06 17:32:20 +0500

    adding baseclass for all primitives

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/BasePrimitive.cs
@@ -0,0 +1,13 @@
+﻿using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal abstract class BasePrimitive : BaseVariable
+    {
+        public PrimitiveType PrimitiveType { get; protected set; } = PrimitiveType.UNKNOWN;
+
+        public BasePrimitive(ushort id, string name) : base(id, name)
+        {
+        }
+    }
+}

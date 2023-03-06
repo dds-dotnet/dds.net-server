371177daa39274d49e13bd82448406314b5a2845
usama <mahmoodify@gmail.com>
2023-03-06 17:23:49 +0500

    adding boolean variable

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/BooleanVariable.cs
@@ -0,0 +1,26 @@
+﻿using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class BooleanVariable : BaseVariable
+    {
+        public PrimitiveType PrimitiveType { get; private set; } = PrimitiveType.Boolean;
+
+        public bool Value { get; set; }
+
+        public BooleanVariable(ushort id, string name) : base(id, name)
+        {
+        }
+
+        public override int GetSizeOnBuffer()
+        {
+            return 1;
+        }
+
+        public override void WriteOnBuffer(ref byte[] buffer, ref int offset)
+        {
+            buffer.WriteBoolean(ref offset, Value);
+        }
+    }
+}

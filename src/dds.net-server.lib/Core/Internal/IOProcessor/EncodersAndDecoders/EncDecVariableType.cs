6842a6e93f4a6599736ff4cadc5e3decb15503a4
usama <mahmoodify@gmail.com>
2023-03-07 17:59:31 +0500

    adding r/w for variable type

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/EncodersAndDecoders/EncDecVariableType.cs
@@ -0,0 +1,34 @@
+﻿using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders
+{
+    internal static class EncDecVariableType
+    {
+        //- 
+        //- VariableType
+        //- 
+        public static VariableType ReadVariableType(this byte[] data, ref int offset)
+        {
+            data.ThrowIfNotHavingRequiredBytes(ref offset, 1);
+
+            int v = data[offset++];
+
+            if (v >= 0 && v < (int)VariableType.UnknownVariableType)
+            {
+                return (VariableType)v;
+            }
+
+            return VariableType.UnknownVariableType;
+        }
+        public static void WriteVariableType(this byte[] data, ref int offset, VariableType value)
+        {
+            data.ThrowIfNotHavingRequiredBytes(ref offset, 1);
+
+            data[offset++] = (byte)value;
+        }
+        public static int GetSizeOnBuffer(this VariableType variableType)
+        {
+            return 1;
+        }
+    }
+}

15ae5c658783bae8b9272be1446d74f4ba14fc96
usama <mahmoodify@gmail.com>
2023-03-08 16:53:03 +0500

    making composition list privately settable

--- src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/CompoundVariable.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/CompoundVariable.cs
@@ -4,7 +4,7 @@ namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
 {
     internal class CompoundVariable : BaseVariable
     {
-        public List<BaseVariable> Composition { get; set; } = new();
+        public List<BaseVariable> Composition { get; private set; } = new();
 
         public CompoundVariable(ushort id, string name) : base(id, name)
         {

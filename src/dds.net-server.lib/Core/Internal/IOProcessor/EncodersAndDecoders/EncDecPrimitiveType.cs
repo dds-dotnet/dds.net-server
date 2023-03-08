9a4260280af2e86e345ec3ccc61eb624a546fb2d
usama <mahmoodify@gmail.com>
2023-03-08 15:05:53 +0500

    adding comments and discarding the unrequired element

--- src/dds.net-server.lib/Core/Internal/IOProcessor/EncodersAndDecoders/EncDecPrimitiveType.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/EncodersAndDecoders/EncDecPrimitiveType.cs
@@ -7,6 +7,16 @@ namespace DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders
         //- 
         //- PrimitiveType
         //- 
+
+        /// <summary>
+        /// Reads PrimitiveType from the data buffer and updates the offset past the PrimitiveType
+        /// </summary>
+        /// <param name="data">The buffer containing data</param>
+        /// <param name="offset">offset in the data buffer - updated afterwards to point
+        /// to the next element in the buffer</param>
+        /// <returns>PrimitiveType</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public static PrimitiveType ReadPrimitiveType(this byte[] data, ref int offset)
         {
             data.ThrowIfNotHavingRequiredBytes(ref offset, 1);
@@ -20,13 +30,27 @@ namespace DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders
 
             return PrimitiveType.UnknownPrimitiveType;
         }
+        /// <summary>
+        /// Writes PrimitiveType to the given data buffer
+        /// </summary>
+        /// <param name="data">The buffer containing data</param>
+        /// <param name="offset">Offset in the data buffer - updated afterwards to point
+        /// to the next element in the buffer</param>
+        /// <param name="value">value to be written to the buffer</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public static void WritePrimitiveType(this byte[] data, ref int offset, PrimitiveType value)
         {
             data.ThrowIfNotHavingRequiredBytes(ref offset, 1);
 
             data[offset++] = (byte)value;
         }
-        public static int GetSizeOnBuffer(this PrimitiveType primitiveType)
+        /// <summary>
+        /// Size in bytes PrimitiveType requires on a buffer
+        /// </summary>
+        /// <param name="_"></param>
+        /// <returns>Number of bytes required on the buffer</returns>
+        public static int GetSizeOnBuffer(this PrimitiveType _)
         {
             return 1;
         }

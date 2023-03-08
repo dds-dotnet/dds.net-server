50815497f8948f4ab80189b6f7bf43647275a743
usama <mahmoodify@gmail.com>
2023-03-08 15:11:05 +0500

    adding comments for explaining usage

--- src/dds.net-server.lib/Entities/VariableType.cs
+++ src/dds.net-server.lib/Entities/VariableType.cs
@@ -2,8 +2,17 @@
 {
     public enum VariableType
     {
+        /// <summary>
+        /// Represents the very basic variable type, e.g., string, integer, float, etc.
+        /// </summary>
         Primitive,
+        /// <summary>
+        /// Represents a combination of basic variable types
+        /// </summary>
         Compound,
+        /// <summary>
+        /// Unknown type
+        /// </summary>
         UnknownVariableType
     }
 }

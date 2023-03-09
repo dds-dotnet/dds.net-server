eaeeb4d82e77b2235fc3f46c4eb7e77aa1105ec3
usama <mahmoodify@gmail.com>
2023-03-10 02:21:23 +0500

    adding comments

--- src/dds.net-server.lib/Core/Internal/Interfaces/ISyncQueueWriterEnd.cs
+++ src/dds.net-server.lib/Core/Internal/Interfaces/ISyncQueueWriterEnd.cs
@@ -2,7 +2,15 @@
 {
     internal interface ISyncQueueWriterEnd<T>
     {
+        /// <summary>
+        /// Checks if the queue has enough space to enqueue any data element.
+        /// </summary>
+        /// <returns>True when the queue has empty space, False otherwise.</returns>
         bool CanEnqueue();
+        /// <summary>
+        /// Enqueues the provided data element into the queue.
+        /// </summary>
+        /// <param name="data">The data element</param>
         void Enqueue(T data);
     }
 }

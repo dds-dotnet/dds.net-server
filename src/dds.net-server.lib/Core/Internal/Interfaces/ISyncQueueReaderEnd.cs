3d5fbb7ec26e166235ade927eef1934e37aa71c0
usama <mahmoodify@gmail.com>
2023-03-10 02:27:27 +0500

    adding comments

--- src/dds.net-server.lib/Core/Internal/Interfaces/ISyncQueueReaderEnd.cs
+++ src/dds.net-server.lib/Core/Internal/Interfaces/ISyncQueueReaderEnd.cs
@@ -2,9 +2,21 @@
 {
     internal interface ISyncQueueReaderEnd<T>
     {
+        /// <summary>
+        /// Event is raised as soon as any data element is made available for reading from the queue.
+        /// </summary>
         event EventHandler<T>? DataAvailableForReading;
 
+        /// <summary>
+        /// Checks if the queue has any element to dequeue.
+        /// </summary>
+        /// <returns>True if any element is available, False otherwise.</returns>
         bool CanDequeue();
+        /// <summary>
+        /// Removes first available data element from the queue and returns it.
+        /// Blocks if no data element is available, as long as any data element is made available.
+        /// </summary>
+        /// <returns>The data element.</returns>
         T Dequeue();
     }
 }

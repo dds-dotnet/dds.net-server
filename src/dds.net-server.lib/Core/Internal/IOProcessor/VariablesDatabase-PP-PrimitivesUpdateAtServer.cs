1d1752f0e31b5ff6fff1d0d0abbb254b6084245b
usama <mahmoodify@gmail.com>
2023-03-09 14:19:12 +0500

    adding comments

--- src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-PP-PrimitivesUpdateAtServer.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-PP-PrimitivesUpdateAtServer.cs
@@ -6,9 +6,14 @@ namespace DDS.Net.Server.Core.Internal.IOProcessor
     internal partial class VariablesDatabase
         : SinglePipedConsumer<DataFromClient, DataToClient, VarsDbCommand, VarsDbStatus>
     {
+        /// <summary>
+        /// Processing the PrimitivesUpdateAtServer packet.
+        /// </summary>
+        /// <param name="clientRef">Sender's information / address.</param>
+        /// <param name="data">The data packet.</param>
+        /// <param name="offset">Offset from where packet parsing is to be started.</param>
         private void ProcessPacket_PrimitivesUpdateAtServer(string clientRef, byte[] data, ref int offset)
         {
-            throw new NotImplementedException();
         }
     }
 }

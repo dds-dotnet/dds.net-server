232dfca0f4ea41355c00ea7bfc50ab3f2330b74c
usama <mahmoodify@gmail.com>
2023-03-12 14:35:33 +0500

    adding unknown main variable - placeholder for late acquisition

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/Types/Variable/UnknownVariable.cs
@@ -0,0 +1,30 @@
+﻿using DDS.Net.Server.Entities;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable
+{
+    internal class UnknownVariable : BaseVariable
+    {
+        public UnknownVariable(ushort id, string name) : base(id, name)
+        {
+            VariableType = VariableType.UnknownVariableType;
+        }
+
+        public override int GetTypeSizeOnBuffer()
+        {
+            return 0;
+        }
+
+        public override int GetValueSizeOnBuffer()
+        {
+            return 0;
+        }
+
+        public override void WriteTypeOnBuffer(ref byte[] buffer, ref int offset)
+        {
+        }
+
+        public override void WriteValueOnBuffer(ref byte[] buffer, ref int offset)
+        {
+        }
+    }
+}

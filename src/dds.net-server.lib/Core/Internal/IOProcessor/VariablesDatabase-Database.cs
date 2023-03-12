fdd0db1c339f1848609c0453fef2f33bad9cc8a7
usama <mahmoodify@gmail.com>
2023-03-13 00:57:34 +0500

    assigning unsigned dword

--- src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-Database.cs
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-Database.cs
@@ -677,7 +677,62 @@ namespace DDS.Net.Server.Core.Internal.IOProcessor
         /// <exception cref="Exception"></exception>
         private bool __AssignPrimitiveUnsignedDWord(BasePrimitive variable, uint value, out string errorMessage)
         {
-            throw new NotImplementedException();
+            if (variable is UnsignedQWordVariable usqw)
+            {
+                errorMessage = string.Empty;
+
+                if (usqw.Value != value)
+                {
+                    usqw.Value = value;
+                    return true;
+                }
+
+                return false;
+            }
+            else if (variable is UnsignedDWordVariable usdw)
+            {
+                errorMessage = string.Empty;
+
+                if (usdw.Value != value)
+                {
+                    usdw.Value = value;
+                    return true;
+                }
+
+                return false;
+            }
+            else if (variable is UnsignedWordVariable usw)
+            {
+                errorMessage = string.Empty;
+
+                if (usw.Value != value)
+                {
+                    usw.Value = (ushort)value;
+                    return true;
+                }
+
+                return false;
+            }
+            else if (variable is UnsignedByteVariable usb)
+            {
+                errorMessage = string.Empty;
+
+                if (usb.Value != value)
+                {
+                    usb.Value = (byte)value;
+                    return true;
+                }
+
+                return false;
+            }
+            else
+            {
+                errorMessage =
+                    $"UnsignedDWord value cannot be assigned to a variable " +
+                    $"of type {variable.PrimitiveType}";
+
+                return false;
+            }
         }
         /// <summary>
         /// Assigns variable with given value.
@@ -731,7 +786,7 @@ namespace DDS.Net.Server.Core.Internal.IOProcessor
 
                 if (usb.Value != value)
                 {
-                    usb.Value = (ushort)value;
+                    usb.Value = (byte)value;
                     return true;
                 }
 

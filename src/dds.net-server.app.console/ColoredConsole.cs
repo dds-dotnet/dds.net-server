ac0bec48473934749209321ee2dc8be36fe3cf12
usama <mahmoodify@gmail.com>
2023-03-15 02:20:24 +0500

    adding extension method for printing colored message on console

---
+++ src/dds.net-server.app.console/ColoredConsole.cs
@@ -0,0 +1,22 @@
+﻿namespace DDS.Net.Server.ConsoleApp
+{
+    internal static class ColoredConsole
+    {
+        internal static void PrintColoredLine(
+            this string message,
+            ConsoleColor fgColor = ConsoleColor.White,
+            ConsoleColor bgColor = ConsoleColor.Black)
+        {
+            ConsoleColor beforeFG = Console.ForegroundColor;
+            ConsoleColor beforeBG = Console.BackgroundColor;
+
+            Console.ForegroundColor = fgColor;
+            Console.BackgroundColor = bgColor;
+
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = beforeFG;
+            Console.BackgroundColor = beforeBG;
+        }
+    }
+}

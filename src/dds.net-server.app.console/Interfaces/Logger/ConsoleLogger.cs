e13f85e3ff2b78a2cedcb295f409a84189f542de
usama <mahmoodify@gmail.com>
2023-03-15 01:47:17 +0500

    adding console logger

---
+++ src/dds.net-server.app.console/Interfaces/Logger/ConsoleLogger.cs
@@ -0,0 +1,34 @@
+﻿using DDS.Net.Server.Interfaces;
+
+namespace DDS.Net.Server.ConsoleApp.Interfaces.Logger
+{
+    internal class ConsoleLogger : ILogger
+    {
+        private readonly LogLevel _logLevel;
+
+        public ConsoleLogger(LogLevel logLevel)
+        {
+            _logLevel = logLevel;
+        }
+        public void Error(string message)
+        {
+            Console.WriteLine($"Error: {message}");
+        }
+
+        public void Info(string message)
+        {
+            if (_logLevel == LogLevel.Information)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public void Warning(string message)
+        {
+            if (_logLevel != LogLevel.Error)
+            {
+                Console.WriteLine($"Warning: {message}");
+            }
+        }
+    }
+}

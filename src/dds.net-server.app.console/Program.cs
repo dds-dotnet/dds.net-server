5ac594794d4137fd3f87eef1c7aaae960a1158b1
usama <mahmoodify@gmail.com>
2023-03-15 02:04:42 +0500

    init and running the server app

--- src/dds.net-server.app.console/Program.cs
+++ src/dds.net-server.app.console/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace DDS.Net.Server.ConsoleApp
+﻿using DDS.Net.Server.ConsoleApp.Configuration;
+using DDS.Net.Server.ConsoleApp.Interfaces.Logger;
+using DDS.Net.Server.Entities;
+using DDS.Net.Server.Interfaces;
+using DDS.Net.Server.PublicHelpers;
+
+namespace DDS.Net.Server.ConsoleApp
 {
     internal class Program
     {
+        private static SplitLogger logger =
+                                new(new FileLogger(AppConstants.LOG_FILENAME, LogLevel.Information),
+                                    new ConsoleLogger(LogLevel.Information));
+
+        private static DdsServer server = null!;
+
         static void Main(string[] args)
         {
             Console.WriteLine("DDS.Net Server");
+
+            (bool isEnabled, ServerConfiguration? config) =
+                ConfigurationProvider.GetServerConfiguration(AppConstants.SERVER_01_CONFIG_FILENAME, logger);
+
+            if (isEnabled && config != null)
+            {
+                server = new DdsServer(
+                    config,
+                    ConfigurationProvider.GetVariablesConfiguration(AppConstants.VARIABLES_CONFIG_FILENAME, logger));
+
+                server.Start();
+            }
+            else
+            {
+                server = null!;
+                logger.Error("Failed to initialize the server configuration.");
+                logger.Error("Server is either not enabled, or its configuration cannot be read.");
+            }
+
+            WaitForKey("Press ESC to exit.", ConsoleKey.Escape);
+
+            server?.Stop();
+            logger?.Dispose();
+        }
+
+        private static void WaitForKey(string message, ConsoleKey waitKey, int breakTime = 100)
+        {
+            Console.WriteLine(message);
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+
+                    if (keyPressed.Key == waitKey)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Thread.Sleep(breakTime);
+                    }
+                }
+            }
         }
     }
 }

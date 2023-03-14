df72714d764a6ef2e4b1604fff4883fdd7307b92
usama <mahmoodify@gmail.com>
2023-03-15 01:36:13 +0500

    adding the console app

---
+++ src/dds.net-server.app.console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DDS.Net.Server.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

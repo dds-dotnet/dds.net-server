5558d0333e1632f6890c41a97bf62e5de9227cd4
usama <mahmoodify@gmail.com>
2023-03-15 01:40:42 +0500

    adding loggers

---
+++ src/dds.net-server.app.console/Interfaces/Logger/BlankLogger.cs
@@ -0,0 +1,19 @@
+﻿using DDS.Net.Server.Interfaces;
+
+namespace DDS.Net.Server.ConsoleApp.Interfaces.Logger
+{
+    internal class BlankLogger : ILogger
+    {
+        public void Error(string message)
+        {
+        }
+
+        public void Info(string message)
+        {
+        }
+
+        public void Warning(string message)
+        {
+        }
+    }
+}

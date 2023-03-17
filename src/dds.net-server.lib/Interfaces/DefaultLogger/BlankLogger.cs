5231344fed28400d17639abba875bd531703e777
usama <mahmoodify@gmail.com>
2023-03-17 18:24:58 +0500

    adding default blank logger

---
+++ src/dds.net-server.lib/Interfaces/DefaultLogger/BlankLogger.cs
@@ -0,0 +1,17 @@
+﻿namespace DDS.Net.Server.Interfaces.DefaultLogger
+{
+    public class BlankLogger : ILogger
+    {
+        public void Error(string message)
+        {
+        }
+
+        public void Info(string message)
+        {
+        }
+
+        public void Warning(string message)
+        {
+        }
+    }
+}

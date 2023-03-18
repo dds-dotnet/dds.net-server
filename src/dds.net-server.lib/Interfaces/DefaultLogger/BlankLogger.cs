bfd6e751f82cacb4c3ec129a4f2adfe5ec416975
usama <mahmoodify@gmail.com>
2023-03-18 16:27:52 +0500

    rearranging

--- src/dds.net-server.lib/Interfaces/DefaultLogger/BlankLogger.cs
+++ src/dds.net-server.lib/Interfaces/DefaultLogger/BlankLogger.cs
@@ -5,15 +5,15 @@
     /// </summary>
     public class BlankLogger : ILogger
     {
-        public void Error(string message)
-        {
-        }
-
         public void Info(string message)
         {
         }
 
         public void Warning(string message)
+        {
+        }
+
+        public void Error(string message)
         {
         }
     }

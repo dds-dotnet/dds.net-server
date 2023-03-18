4eb46fa9ed902f0d2ab5e5455f8ebb6d2ff7af7e
usama <mahmoodify@gmail.com>
2023-03-18 17:46:44 +0500

    adding documentation comments

--- src/dds.net-server.lib/Entities/ServerConfiguration.cs
+++ src/dds.net-server.lib/Entities/ServerConfiguration.cs
@@ -2,19 +2,54 @@
 
 namespace DDS.Net.Server.Entities
 {
+    /// <summary>
+    /// Class <c>ServerConfiguration</c> represents configuration for server working.
+    /// </summary>
     public class ServerConfiguration
     {
+        /// <summary>
+        /// The listening IPv4 address for receiving connections and data.
+        /// </summary>
         public string ListeningAddressIPv4 { get; }
 
+        /// <summary>
+        /// Should the TCP connectivity be enabled or not.
+        /// </summary>
         public bool EnableTCP { get; }
+        /// <summary>
+        /// The local TCP port for receiving connections.
+        /// </summary>
         public ushort ListeningPortTCP { get; }
+        /// <summary>
+        /// The maximum number of TCP clients.
+        /// </summary>
         public int MaxClientsTCP { get; }
 
+        /// <summary>
+        /// Should the UDP connectivity be enabled or not.
+        /// </summary>
         public bool EnableUDP { get; }
+        /// <summary>
+        /// The local UDP port for receiving data.
+        /// </summary>
         public ushort ListeningPortUDP { get; }
 
+        /// <summary>
+        /// The implementation of <c>ILogger</c> interface that should
+        /// be used by the server.
+        /// </summary>
         public ILogger Logger { get; }
 
+        /// <summary>
+        /// Initializes the configuration object.
+        /// </summary>
+        /// <param name="listeningIPv4Address">The listening IPv4 address for receiving connections and data.</param>
+        /// <param name="enableTCP">Should the TCP connectivity be enabled or not.</param>
+        /// <param name="tcpPort">The local TCP port for receiving connections.</param>
+        /// <param name="tcpMaxClients">The maximum number of TCP clients.</param>
+        /// <param name="enableUDP">Should the UDP connectivity be enabled or not.</param>
+        /// <param name="udpPort">The local UDP port for receiving data.</param>
+        /// <param name="logger">The implementation of <c>ILogger</c> interface that should be used by the server.</param>
         public ServerConfiguration(
             string listeningIPv4Address,
             bool enableTCP, ushort tcpPort, int tcpMaxClients,

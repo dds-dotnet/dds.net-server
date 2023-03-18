65a7e7dd8df32ac70209b2732def8571981ea48b
usama <mahmoodify@gmail.com>
2023-03-18 17:32:01 +0500

    adding doc for base settings class

--- src/dds.net-server.lib/Entities/BaseVariableSettings.cs
+++ src/dds.net-server.lib/Entities/BaseVariableSettings.cs
@@ -1,9 +1,19 @@
 ﻿namespace DDS.Net.Server.Entities
 {
+    /// <summary>
+    /// Base class for variable settings.
+    /// </summary>
     public class BaseVariableSettings
     {
+        /// <summary>
+        /// Name of the variable.
+        /// </summary>
         public string VariableName { get; private set; }
 
+        /// <summary>
+        /// Initializes the class with given parameters.
+        /// </summary>
+        /// <param name="name">The name of the variable.</param>
         public BaseVariableSettings(string name)
         {
             VariableName = name;

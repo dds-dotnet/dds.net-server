d2e77a7d8ef382a10a3729aeb1c404b9bb1eef69
usama <mahmoodify@gmail.com>
2023-03-18 16:42:03 +0500

    adding extension method for printing without endline

--- src/dds.net-server.lib/Interfaces/DefaultLogger/ConsoleExtension.cs
+++ src/dds.net-server.lib/Interfaces/DefaultLogger/ConsoleExtension.cs
@@ -30,5 +30,31 @@
                 Console.BackgroundColor = beforeBG;
             }
         }
+        /// <summary>
+        /// Extension method to output colored text without a line-end on standard console
+        /// and restores previously set colors.
+        /// </summary>
+        /// <param name="message">Text to output.</param>
+        /// <param name="fgColor">Text color.</param>
+        /// <param name="bgColor">Background color.</param>
+        public static void Write(
+            this string message,
+            ConsoleColor fgColor = ConsoleColor.White,
+            ConsoleColor bgColor = ConsoleColor.Black)
+        {
+            lock (mutex)
+            {
+                ConsoleColor beforeFG = Console.ForegroundColor;
+                ConsoleColor beforeBG = Console.BackgroundColor;
+
+                Console.ForegroundColor = fgColor;
+                Console.BackgroundColor = bgColor;
+
+                Console.WriteLine(message);
+
+                Console.ForegroundColor = beforeFG;
+                Console.BackgroundColor = beforeBG;
+            }
+        }
     }
 }

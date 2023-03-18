f7ff654be7a1205bfadfe94188adb355637349a0
usama <mahmoodify@gmail.com>
2023-03-18 17:03:07 +0500

    adding doc comments

--- src/dds.net-server.lib/PublicExtensions/FileExtensions.cs
+++ src/dds.net-server.lib/PublicExtensions/FileExtensions.cs
@@ -1,7 +1,17 @@
 ﻿namespace DDS.Net.Server.PublicExtensions
 {
+    /// <summary>
+    /// Provides extension methods for working with files, folders and paths.
+    /// </summary>
     public static class FileExtensions
     {
+        /// <summary>
+        /// Always returns a list of strings containing parts of a given
+        /// string split by specified character.
+        /// </summary>
+        /// <param name="str">The input string that needs to be split.</param>
+        /// <param name="splitter">The character to split the string over.</param>
+        /// <returns></returns>
         public static List<string> TrimmedParts(this string str, char splitter)
         {
             if (string.IsNullOrEmpty(str) == false)
@@ -17,6 +27,10 @@
             return new List<string>();
         }
 
+        /// <summary>
+        /// Creates all the folders (if not existing) for putting a file with specified relative path.
+        /// </summary>
+        /// <param name="filename">Relative path of file.</param>
         public static void CreateFoldersForRelativeFilename(this string filename)
         {
             if (string.IsNullOrEmpty(filename)) return;

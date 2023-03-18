9622e1995d465b692f2af2cd926a0cb56f548cef
usama <mahmoodify@gmail.com>
2023-03-18 17:52:08 +0500

    adding doc comments

--- src/dds.net-server.lib/Entities/VariablesConfiguration.cs
+++ src/dds.net-server.lib/Entities/VariablesConfiguration.cs
@@ -1,9 +1,22 @@
 ﻿namespace DDS.Net.Server.Entities
 {
+    /// <summary>
+    /// The class <c>VariablesConfiguration</c> represents starting configuration
+    /// for variables.
+    /// </summary>
     public class VariablesConfiguration
     {
+        /// <summary>
+        /// List of settings for individual variables.
+        /// </summary>
         public List<BaseVariableSettings> Settings { get; private set; } = new();
 
+        /// <summary>
+        /// Adds a new settings object to the list. Overrides previous variable
+        /// settings object (if any available) that has the same name as
+        /// the newly provided variable settings object.
+        /// </summary>
+        /// <param name="settings">The new settings object.</param>
         public void AddVariableSettings(BaseVariableSettings settings)
         {
             foreach (var setting in Settings)

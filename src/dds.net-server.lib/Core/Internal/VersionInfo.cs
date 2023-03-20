13959ee74c0b7ff472cdd981c01ef8ade16e6499
usama <mahmoodify@gmail.com>
2023-03-20 15:50:02 +0500

    version update

--- src/dds.net-server.lib/Core/Internal/VersionInfo.cs
+++ src/dds.net-server.lib/Core/Internal/VersionInfo.cs
@@ -3,6 +3,6 @@
     internal class VersionInfo
     {
         public static readonly string SERVER_NAME = "DDS.Net Server";
-        public static readonly string SERVER_VERSION = "1.0.0";
+        public static readonly string SERVER_VERSION = "1.0.1";
     }
 }

95228b53fd2e4c7940cbf30560b08f2ac36ec0f2
usama <mahmoodify@gmail.com>
2023-03-23 02:47:42 +0500

    adding placeholder for data out routines

---
+++ src/dds.net-server.lib/Core/Internal/IOProcessor/VariablesDatabase-DataOut.cs
@@ -0,0 +1,16 @@
+﻿using DDS.Net.Server.Core.Internal.Base;
+using DDS.Net.Server.Core.Internal.Base.Entities;
+using DDS.Net.Server.Core.Internal.IOProcessor.EncodersAndDecoders;
+using DDS.Net.Server.Core.Internal.IOProcessor.Types;
+using DDS.Net.Server.Core.Internal.IOProcessor.Types.Variable;
+using DDS.Net.Server.Entities;
+using System.Text;
+
+namespace DDS.Net.Server.Core.Internal.IOProcessor
+{
+    internal partial class VariablesDatabase
+        : SinglePipedConsumer<DataFromClient, DataToClient, VarsDbCommand, VarsDbStatus>
+    {
+        
+    }
+}
